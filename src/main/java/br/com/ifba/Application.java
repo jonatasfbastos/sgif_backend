74f091743e3f01e4fdd575d0dea44b027c21c19d
VitorDanielA <vdanieldev1@gmail.com>
2023-04-18 13:02:27 -0300

    Ajustando application

--- src/main/java/br/com/ifba/Application.java
+++ src/main/java/br/com/ifba/Application.java
@@ -3,11 +3,12 @@ package br.com.ifba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
 	public static void main(String[] args) {

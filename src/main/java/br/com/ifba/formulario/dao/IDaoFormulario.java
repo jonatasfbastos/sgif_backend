fbf1c6b882314f00d137bbf5871f350e3816ba74
Lucas Nogueira Bonfim Bastos <lucasnbbastos@outlook.com>
2023-05-13 21:30:52 -0300

    Remoção do @Repository do Dao Formulário

--- src/main/java/br/com/ifba/formulario/dao/IDaoFormulario.java
+++ src/main/java/br/com/ifba/formulario/dao/IDaoFormulario.java
@@ -3,11 +3,9 @@ package br.com.ifba.formulario.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.ifba.formulario.model.Formulario;
 
-@Repository
 public interface IDaoFormulario extends JpaRepository<Formulario, Long>{
     
     public abstract List<Formulario> findByTitulo(String titulo);

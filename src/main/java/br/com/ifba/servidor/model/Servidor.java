a14c712e6db28f15d112ae69e6bc8f8a7c30fdb9
Gustavo Dourado <gusd.batista@gmai.com>
2023-05-16 11:51:39 -0300

    removendo funcaoServidor e servidor para corecao

--- src/main/java/br/com/ifba/servidor/model/Servidor.java
+++ src/main/java/br/com/ifba/servidor/model/Servidor.java
@@ -2,10 +2,7 @@ package br.com.ifba.servidor.model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import br.com.ifba.funcaoservidor.model.FuncaoServidor;
 import br.com.ifba.infrastructure.model.PersistenceEntity;
 import br.com.ifba.servidor.model.Servidor;
 
@@ -13,10 +10,6 @@ import br.com.ifba.servidor.model.Servidor;
 @Table(name = "servidor")
 
 public abstract class Servidor extends PersistenceEntity implements Serializable{
-
-    @ManyToOne
-    @JoinColumn(name="funcao_servidor", nullable=false)
-    private FuncaoServidor funcaoServidor;
     private String nome;
     private String siape;
 

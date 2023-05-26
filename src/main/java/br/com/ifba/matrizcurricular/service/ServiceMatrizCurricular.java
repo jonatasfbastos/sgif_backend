73bf88d2f6d1334fed3520116afacbb6615cfb3b
João Paulo <costajoaopaulo113@gmail.com>
2023-05-26 07:26:39 -0300

    Corrigindo condição de erro em M. Curricular

--- src/main/java/br/com/ifba/matrizcurricular/service/ServiceMatrizCurricular.java
+++ src/main/java/br/com/ifba/matrizcurricular/service/ServiceMatrizCurricular.java
@@ -37,6 +37,7 @@ public class ServiceMatrizCurricular implements IServiceMatrizCurricular{
      //================= OBJETO =================================================
     @Autowired
     private IDaoMatrizCurricular matrizCurricularDao;
+    @Autowired
     private IDaoEtapaCurso etapaCursoDao;
 
      //================= MÃ‰TODOS ================================================
@@ -76,7 +77,7 @@ public class ServiceMatrizCurricular implements IServiceMatrizCurricular{
         if(matrizCurricularDao.existsById(matrizCurricular.getId()) == false) {
             throw new BusinessException(MATRIZ_CURRICULAR_NAO_EXISTE);
         }
-        if(matrizCurricularDao.equals(etapaCursoDao) == true) {
+        if(matrizCurricularDao.getReferenceById(matrizCurricular.getId()).getEtapacurso().isEmpty() == false) {
             throw new BusinessException(ETAPA_CURSO_EXISTE);
         }
         matrizCurricularDao.delete(matrizCurricular);

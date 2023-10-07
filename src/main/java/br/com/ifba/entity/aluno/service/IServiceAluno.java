e092fddc09cacb01075fa9840188cd75cbf0c3c8
NekoYasha <autornekoyasha@gmail.com>
2023-10-07 13:56:26 -0300

    ✏ refactor: Renomeando métodos e comentando.
    Principais mudanças:
    1. Métodos renomeados para seguir o padrão de linguagem (português brasileiro)
    2. Comentários adicionados.

--- src/main/java/br/com/ifba/entity/aluno/service/IServiceAluno.java
+++ src/main/java/br/com/ifba/entity/aluno/service/IServiceAluno.java
@@ -5,15 +5,54 @@ import java.util.List;
 import br.com.ifba.entity.aluno.model.Aluno;
 
 public interface IServiceAluno {
-    
-    public abstract Aluno saveAluno(Aluno professor);
 
-    public abstract Aluno updateAluno(Aluno professor);
+    // =========================================================== //
+    // =============== [ ASSINATURAS DE MÉTODOS ] ================ //
+    // =========================================================== //
 
-    public abstract void deleteAluno(Aluno professor);
+    /**
+     *
+     * Salva um aluno na base de ddados.
+     *
+     * @param aluno - O Aluno que será salvo na base de dados.
+     * @return os dados do aluno salvo na base de dados,
+     * caso a operação ocorra com sucesso.
+     *
+     */
+    Aluno salvarAluno(Aluno aluno);
 
-    public abstract List<Aluno> getAllaluno();
-    
-    public List<Aluno> findByStatusAlunoId(Long id);
+    /**
+     *
+     * Atualiza os dados de uma aluno existente na base de dados.
+     *
+     * @param aluno - O Aluno que terá os seus dados atualizados.
+     * @return os dados atualizados do aluno.
+     *
+     */
+    Aluno atualizarAluno(Aluno aluno);
+
+    /**
+     *
+     * Deleta um aluno da base de dados.
+     *
+     * @param aluno - O Aluno que será deletado da base de dados.
+     *
+     */
+    void deletarAluno(Aluno aluno);
+
+    /**
+     *
+     * Lista todos os alunos presentes na base de dados.
+     *
+     * @return uma lista com todos os alunos da base de dados, ou nulo caso não existe nenhum.
+     */
+    List<Aluno> listarAlunos();
+
+    /**
+     *
+     * AVISO: Temporariamente desabilitada enquanto não é entendida a sua utilidade.
+     *
+     */
+    List<Aluno> findByStatusAlunoId(Long id);
 
 }
--- src/main/java/br/com/ifba/entity/aluno/service/ServiceAluno.java
+++ src/main/java/br/com/ifba/entity/aluno/service/ServiceAluno.java
@@ -12,23 +12,52 @@ import br.com.ifba.infrastructure.exception.BusinessException;
 @Service
 public class ServiceAluno implements IServiceAluno{
     
-    // Mensagem de erro se o Aluno for null.
+
+    // =========================================================== //
+    // =============== [       ATRIBUTOS        ] ================ //
+    // =========================================================== //
+
+    /**
+     * Mensagem de erro se o aluno for nulo.
+     */
     public final static String ALUNO_NULL = "Dados do Aluno nao preenchidos";
-    // Mensagem de erro se o Aluno jÃ¡ existir.
+
+    /**
+     * Mensagem de erro para caso o aluno já exista na base de dados.
+     */
     public final static String ALUNO_EXISTE = "Aluno ja existente no Banco de dados";
-    // Mensagem de erro se o Aluno nÃ£o existir no banco.
-    public final static String ALUNO_NAO_EXISTE = "Aluno nao existente no Banco de dados";
-    // Mensagem de erro se o Aluno for invÃ¡lido.
-    public final static String ALUNO_INVALIDO = "As informaÃ§oes do Aluno nao sao validas";
-     // Mensagem de erro caso o nome esteja vazio.
-    
-     //================= OBJETO =================================================
+
+    /**
+     * Mensagem de erro para caso o aluno não exista na base de dados.
+     */
+    public final static String ALUNO_NAO_EXISTE = "Aluno não existente no Banco de dados";
+
+    /**
+     * Mensagem de erro para caso o aluno tenha informações inválidas.
+     */
+    public final static String ALUNO_INVALIDO = "As informações do Aluno não são válidas";
+
+    /**
+     * Reposítório da entidade 'aluno'.
+     */
     @Autowired
     private IDaoAluno alunoDao;
 
-     //================= MÃ‰TODOS ================================================
+    // =========================================================== //
+    // =============== [       MÉTODOS          ] ================ //
+    // =========================================================== //
+
+    /**
+     *
+     * Salva um aluno na base de ddados.
+     *
+     * @param aluno - O Aluno que será salvo na base de dados.
+     * @return os dados do aluno salvo na base de dados,
+     * caso a operação ocorra com sucesso.
+     *
+     */
     @Override
-    public Aluno saveAluno(Aluno aluno) {
+    public Aluno salvarAluno(Aluno aluno) {
        if(aluno == null) {
             throw new BusinessException(ALUNO_NULL);
        } else{
@@ -36,8 +65,16 @@ public class ServiceAluno implements IServiceAluno{
        }
     }
 
+    /**
+     *
+     * Atualiza os dados de uma aluno existente na base de dados.
+     *
+     * @param aluno - O Aluno que terá os seus dados atualizados.
+     * @return os dados atualizados do aluno.
+     *
+     */
     @Override
-    public Aluno updateAluno(Aluno aluno) {
+    public Aluno atualizarAluno(Aluno aluno) {
         if(aluno == null) {
             throw new BusinessException(ALUNO_NULL);
         } 
@@ -47,8 +84,15 @@ public class ServiceAluno implements IServiceAluno{
         return alunoDao.save(aluno);
     }
 
+    /**
+     *
+     * Deleta um aluno da base de dados.
+     *
+     * @param aluno - O Aluno que será deletado da base de dados.
+     *
+     */
     @Override
-    public void deleteAluno(Aluno aluno) {
+    public void deletarAluno(Aluno aluno) {
         if(aluno == null) {
             throw new BusinessException(ALUNO_NULL);
         } 
@@ -58,11 +102,20 @@ public class ServiceAluno implements IServiceAluno{
         alunoDao.delete(aluno);
     }
 
+    /**
+     *
+     * Lista todos os alunos presentes na base de dados.
+     *
+     * @return uma lista com todos os alunos da base de dados, ou nulo caso não existe nenhum.
+     */
     @Override
-    public List<Aluno> getAllaluno() {
+    public List<Aluno> listarAlunos() {
         return this.alunoDao.findAll();
     }
 
+    /**
+     * AVISO: Temporariamente desabilitada enquanto não é entendida a sua utilidade.
+     */
     @Override
     public List<Aluno> findByStatusAlunoId(Long id) {
         return alunoDao.findByStatusAlunoId(id);

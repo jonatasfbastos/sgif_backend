8aadf6befb7e37afd46af60de4c56cfb342a7537
NekoYasha <autornekoyasha@gmail.com>
2023-10-07 09:15:51 -0300

    📜 feat: Criando diretório 'security'.
    > Classe 'WebSecurityConfig' de segurança foi movida para o diretório 'security', para fins de organização do código.

--- src/main/java/br/com/ifba/security/WebSecurityConfig.java
+++ src/main/java/br/com/ifba/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package br.com.ifba;
+package br.com.ifba.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

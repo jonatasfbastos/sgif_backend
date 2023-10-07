6b4d21a2cedb3101e1a215ae1697150de19ab451
NekoYasha <autornekoyasha@gmail.com>
2023-10-07 14:19:26 -0300

    📜 feat: Criando controller para aluno.
    > Endpoint criado para requisições direcionadas à entidade 'aluno'.

---
+++ src/main/java/br/com/ifba/controller/AlunoController.java
@@ -0,0 +1,47 @@
+package br.com.ifba.controller;
+
+import br.com.ifba.entity.aluno.model.Aluno;
+import br.com.ifba.entity.aluno.service.IServiceAluno;
+
+import com.google.gson.Gson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class AlunoController {
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+    Gson gson = new Gson();
+
+    @Autowired
+    private IServiceAluno serviceAluno;
+
+    // =========================================================== //
+    // =============== [         ENDPOINTS      ] ================ //
+    // =========================================================== //
+
+    @GetMapping(path = "/alunos")
+    public ResponseEntity<?> listarAlunos() {
+        return ResponseEntity.status(HttpStatus.OK).body(serviceAluno.listarAlunos());
+    }
+
+    @PostMapping(path = "/alunos/aluno")
+    public ResponseEntity<?> salvarAluno(@RequestBody String aluno1) {
+        Aluno aluno = (Aluno) gson.fromJson(aluno1, Aluno.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(serviceAluno.salvarAluno(aluno));
+    }
+
+    @DeleteMapping(path = "/alunos/aluno/{id}")
+    public boolean deletarAluno(@PathVariable("id") Long id) {
+        Aluno aluno = new Aluno();
+        aluno.setId(id);
+        serviceAluno.deletarAluno(aluno);
+        return true;
+    }
+}

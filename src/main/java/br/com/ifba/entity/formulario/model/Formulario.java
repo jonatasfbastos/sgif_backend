baaaafbdbc25dfe93ca4fdb4fb40ab2b501e034c
NekoYasha <autornekoyasha@gmail.com>
2023-10-07 11:39:02 -0300

    ✏ refactor: Editando e comentando formulário
    Princiais modificações na classe:
    1. Adição de comentários.
    2. Adição da instância da lista de questões, com uma relação 'OneToMany'.
    3. Adição da anotação 'Column'.

--- src/main/java/br/com/ifba/entity/formulario/model/Formulario.java
+++ src/main/java/br/com/ifba/entity/formulario/model/Formulario.java
@@ -2,25 +2,47 @@ package br.com.ifba.entity.formulario.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import br.com.ifba.entity.avaliacao.model.Avaliacao;
+import br.com.ifba.entity.questao.model.Questao;
 import br.com.ifba.infrastructure.model.PersistenceEntity;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity
-@Table(name = "formulario")
+import java.util.List;
+
+/**
+ * Esta classe representa um formulário que contém um título,
+ * uma descrição e uma lista de questões associadas.
+ */
+@Entity(name = "formulario")
+@Table(name = "formularios")
 @Data
 @EqualsAndHashCode(callSuper = false)
-
 public class Formulario extends PersistenceEntity implements Serializable{
-    
+
+    /**
+     * O título do formuláro, não aceita valores nulos.
+     */
+    @Column(name = "titulo")
     private String titulo;
+
+    /**
+     * A descrição do formulário, não aceita valores nulos.
+     */
+    @Column(name = "descricao")
     private String descricao;
-    
-    @OneToOne
-    private Avaliacao avaliacao;
+
+    /**
+     * A lista de questões do formulário, todas as questões são obirgatórias.
+     * Uma questão é composta por um enunciado e uma resposta.
+     */
+    @OneToMany
+    @JoinColumn(name = "questao_id")
+    private List<Questao> questoes;
 }

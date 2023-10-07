8eea522cc8ba62cc152c163d62c6c966e0b7ff38
NekoYasha <autornekoyasha@gmail.com>
2023-10-07 13:30:55 -0300

    ✏ refactor: Editando e comentando classe
    Principais modificações na classe:
    1. Adição de comentários.
    2. Adição da anotação 'Column'.
    3. Adição do parâmetro nome à anotação 'Entity'.
    4. Nome da tabela mudado de 'aluno' para 'alunos'.

--- src/main/java/br/com/ifba/entity/aluno/model/Aluno.java
+++ src/main/java/br/com/ifba/entity/aluno/model/Aluno.java
@@ -2,6 +2,7 @@ package br.com.ifba.entity.aluno.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -11,20 +12,37 @@ import javax.persistence.Table;
 import br.com.ifba.entity.pessoa.model.Pessoa;
 import br.com.ifba.entity.statusaluno.model.StatusAluno;
 import br.com.ifba.entity.turma.model.Turma;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity
-@Table(name = "aluno")
+/**
+ * Esta classe, descendente da classe Pesso, representa um aluno.
+ * Nenhum dos atributos desta classes pode ser nulo.
+ */
+@Entity(name = "aluno")
+@Table(name = "alunos")
 @Data
 @EqualsAndHashCode(callSuper = false)
-
 public class Aluno extends Pessoa implements Serializable{
+
+    /**
+     * A matrícula do aluno, ela possui padronização,
+     * é única e não pode ser nula.
+     */
+    @Column(name = "matricula", nullable = false, unique = true)
     private String matricula;
+
+    /**
+     * A turma do aluno.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "turma_id", referencedColumnName = "ID")
     private Turma turma;
-    
+
+    /**
+     * O status do aluno.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "status_id", referencedColumnName = "ID")
     private StatusAluno statusAluno;

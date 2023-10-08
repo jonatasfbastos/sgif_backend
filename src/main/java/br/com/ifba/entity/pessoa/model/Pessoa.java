8ad9a140b5928ad1aff267a4879e064c21280867
NekoYasha <autornekoyasha@gmail.com>
2023-10-08 12:24:39 -0300

    ✏ refactor: Editando e comentando classe
    Principais modificações do código:
    1. Adicionada anotação 'Column'.
    2. Comentários adicionados.
    3.

--- src/main/java/br/com/ifba/entity/pessoa/model/Pessoa.java
+++ src/main/java/br/com/ifba/entity/pessoa/model/Pessoa.java
@@ -1,41 +1,73 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.pessoa.model;
 
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
 import br.com.ifba.entity.requisicao.model.Requisicao;
-import javax.persistence.Temporal;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
  *
  * @author matheus
+ *
+ * Esta classe represante uma pessoa, um ascendente de diversas outras classes do sistema.
  */
-
-@Entity
-@Table(name = "pessoa")
+@Entity(name = "pessoa")
+@Table(name = "pessoas")
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Pessoa extends PersistenceEntity {
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+
+    /**
+     * O nome da pessoa, não pode ser nulo.
+     */
+    @Column(name = "nome", nullable = false)
     private String nome;
+
+    /**
+     * O telefone da pessoa,
+     * exemplo: (na tabela) 74999999999 -> (com máscara) (74) 99999-9999
+     */
+    @Column(name = "telefone")
     private String telefone;
+
+    /**
+     * O email da pessoa.
+     */
+    @Column(name = "email")
     private String email;
+
+    /**
+     * O CPF (Cadastro de Pessoa Física) da pessoa,
+     * não pode ser nulo e deve ser único.
+     * Exemplo: (na tabela) 12345678900 -> (com máscara) 123.456.789-00
+     */
+    @Column(name = "cpf", nullable = false, unique = true)
     private String cpf;
-    
+
+    /**
+     * A data de nascimento da pessoa.
+     */
+    @Column(name = "data_nascimento")
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeNascimento;
 
+    /**
+     * A lista com requisições da pessoa.
+     */
     @OneToMany(mappedBy = "requisitante")
     private List<Requisicao> requisicoes;
 }

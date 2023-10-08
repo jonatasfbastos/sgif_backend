29aaeead67f3c39b2e7c2a79ef8216adf344bcc5
andesson reis <andessonreys@gmail.com>
2023-10-08 14:02:31 -0300

    🎨 style: Adicionando comentários padronizados

--- src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
+++ src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
@@ -1,24 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.usuario.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import br.com.ifba.infrastructure.model.PersistenceEntity;
 import br.com.ifba.entity.perfilusuario.model.PerfilUsuario;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- *
- * @author vitor
+ * Esta classe, descendente da classe Pessoa, representa um usuario.
+ * Nenhum dos atributos desta classes pode ser nulo.
  */
 
 @Entity
@@ -26,15 +27,27 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Usuario extends PersistenceEntity {
-    private String login;
-    private String senha;
 
+    /**
+     * O nome de usuário do usuário. Não pode ser nulo.
+     */
+
+    @Column(nullable = false, unique = true)
+    private String login;
+    
+    /**
+     * A senha do usuário. Não pode ser nula.
+     */
+
+    @NotNull
+    private String password;
+
+    /**
+     * Perfil do usuario.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "perfil_usuario_id", referencedColumnName = "ID")
     @JsonIgnoreProperties("usuarios")
     private PerfilUsuario perfilUsuario;
-
-    // @OneToOne(fetch = FetchType.LAZY)
-    // private Pessoa pessoa;
 
 }

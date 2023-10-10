9004ee1357bc344c7e1733b87ba23728bf460f54
andesson reis <andessonreys@gmail.com>
2023-10-10 10:58:54 -0300

    feat: Adiciona @NotBlank para validação de campo de login
    A anotação @NotBlank foi adicionada ao campo 'login' para garantir que ele não seja vazio (em branco). Isso melhora a validação do formulário de login e ajuda a evitar entradas inválidas."

--- src/main/java/br/com/ifba/entity/usuario/dto/UsuarioRequestDto.java
+++ src/main/java/br/com/ifba/entity/usuario/dto/UsuarioRequestDto.java
@@ -2,6 +2,7 @@ package br.com.ifba.entity.usuario.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -22,13 +23,13 @@ public record UsuarioRequestDto(
 
         @JsonProperty(value = "login")
         @NotNull(message = "O login é obrigatório.")
-        @NotNull(message = "O login não pode ser vazio.")
+        @NotBlank(message = "O login não pode ser vazio.")
         @Size(min = 3, max = 150, message = "O login precisa ter pelo menos 3 caracteres e 150 no máximo.")
         String login,
 
         @JsonProperty(value = "senha")
         @NotNull(message = "A senha é obrigatória.")
-        @NotNull(message = "A senha não pode ser vazia.")
+        @NotBlank(message = "A senha não pode ser vazia.")
         String senha
 
 ) {

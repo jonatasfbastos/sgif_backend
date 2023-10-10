e365ae8198eddbf29e97b8e2bd047973c699ba82
NekoYasha <autornekoyasha@gmail.com>
2023-10-10 08:54:11 -0300

    ✏ refactor Editando classe
    Principais modificações:
    1. Comentários adicionados.
    2. Novos atributos adicionados.
    3. Anotações 'Entity' e 'Table' editadas.

--- src/main/java/br/com/ifba/entity/questao/model/Questao.java
+++ src/main/java/br/com/ifba/entity/questao/model/Questao.java
@@ -2,18 +2,28 @@ package br.com.ifba.entity.questao.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import br.com.ifba.entity.formulario.model.Formulario;
+import br.com.ifba.entity.resposta.model.Resposta;
 import br.com.ifba.infrastructure.model.PersistenceEntity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.List;
+
 /**
  * Esta classe representa uma questão de um formulário.
  * Ela contém informações sobre o enunciado da questão e a resposta, sendo que a resposta inicialmente é nula.
+ *
+ * veja também: {@link br.com.ifba.entity.formulario.model.Formulario}
  */
 @Entity(name = "questao")
 @Table(name = "questoes")
@@ -28,12 +38,18 @@ public class Questao extends PersistenceEntity implements Serializable{
     private String enunciado;
 
     /**
-     * AVISO: futuramente este atributo será a instância de uma lista da classe 'Resposta',
-     * pois será necessário identificar o conteúdo da reposta e o dono da resposta.
-     * No contexto atual, não é considerada a hipotese que uma questão,
-     * que será distribuida pora diversos usuários, possui mais de uma resposta.
+     * O formulario ao qual a questão pertence.
      */
-    @Column(name = "resposta", nullable = false)
-    private String resposta;
+    @ManyToOne
+    @JoinColumn(name = "formulario_id", referencedColumnName = "ID")
+    private Formulario formulario;
+
+    /**
+     * A lista de respostas da questão,
+     * cada resposta tem o seu dono, ou seja, quem respondeu.
+     */
+    @Column(name = "respostas", nullable = false)
+    @OneToMany(mappedBy = "questão", cascade = CascadeType.ALL)
+    private List<Resposta> respostas;
     
 }

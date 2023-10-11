793675bb74084eb44195e8ff10f0ebb17aaaca5c
NekoYasha <autornekoyasha@gmail.com>
2023-10-11 19:37:13 -0300

    🎨 style: Editando comentários

--- src/main/java/br/com/ifba/entity/perfilusuario/dto/PerfilUsuarioRequestDto.java
+++ src/main/java/br/com/ifba/entity/perfilusuario/dto/PerfilUsuarioRequestDto.java
@@ -1,2 +1,11 @@
-package br.com.ifba.entity.perfilusuario.dto;public record PerfilUsuarioRequestDto() {
+package br.com.ifba.entity.perfilusuario.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record PerfilUsuarioRequestDto(
+        @JsonProperty(value = "nome")
+        String nome,
+        @JsonProperty(value = "descricao")
+        String descricao
+) {
 }

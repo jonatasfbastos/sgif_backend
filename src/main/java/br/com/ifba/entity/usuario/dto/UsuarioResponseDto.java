294a84ad0c430d9f49b02bca2d641678676798d6
andesson reis <andessonreys@gmail.com>
2023-10-11 11:43:10 -0300

    refactor: Remover anotações '@' não utilizadas

--- src/main/java/br/com/ifba/entity/usuario/dto/UsuarioResponseDto.java
+++ src/main/java/br/com/ifba/entity/usuario/dto/UsuarioResponseDto.java
@@ -2,20 +2,34 @@ package br.com.ifba.entity.usuario.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioRequestDto;
+import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
 
 /**
+ * DTO para resposta às requisições direcionadas à entidade 'Usuário'.
+ * Este DTO representa informações públicas do usuário, incluindo o login.
  *
- * @author Andesson Reis
- *
- * DTO para resposta às requisições direcionadas à entidade 'usuario'.
- * @param username
- * @param perfilUsuario
+ * @param login - O login do usuário (username)
+ * @param senha - A senha do usuário
+ * 
+ * Veja também: {@link br.com.ifba.entity.usuario.model.Usuario}
+ * 
  */
 
- public record UsuarioResponseDto(
-    @JsonProperty(value = "username")
-    String username,
-    @JsonProperty(value = "perfilUsuario")
-    PerfilUsuarioRequestDto perfilUsuario
- ){}
+public record UsuarioResponseDto(
+            /**
+             * O login do usuário (username).
+             */
+            @JsonProperty(value = "login")
+            String login,
+            /**
+             * A senha do usuário.
+             * AVISO: A senha será removida ou criptografada posteriormente por motivos de
+             * segurança.
+             */
+            @JsonProperty(value = "senha") 
+            String senha,
+
+            @JsonProperty(value = "perfilUsuario") 
+            PerfilUsuarioResponseDto perfilUsuario
+            ) {
+}

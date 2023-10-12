bd0f0c8f7cf583788f877b676070dd8a73e536cc
NekoYasha <autornekoyasha@gmail.com>
2023-10-12 19:29:03 -0300

    ✏ refactor: Editando atributo 'resposta'
    > Atributo resposta mudado para uma lista de DTO do objeto 'RespostaRequestDto'.

--- src/main/java/br/com/ifba/entity/questao/dto/QuestaoRequestDto.java
+++ src/main/java/br/com/ifba/entity/questao/dto/QuestaoRequestDto.java
@@ -6,16 +6,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import java.util.List;
+
 /**
  *
  * @author Giovane Neves
+ * Desde V1.0.1
  *
  * DTO que recebe dados de requisições direcionadas à entidade 'Questão'.
  *
  * @param enunciado - O enunciado da questão.
- * @param resposta - A resposta da questão
+ * @param respostas - As respostas da questão
  *
  * Veja também: {@link br.com.ifba.entity.questao.model.Questao}
+ * Veja também: {@link br.com.ifba.entity.resposta.dto.RespostaRequestDto}
  *
  */
 public record QuestaoRequestDto(
@@ -24,9 +28,8 @@ public record QuestaoRequestDto(
     @NotBlank(message = "O enunciado não pode ser vazio.")
     String enunciado,
 
-    @JsonProperty(value = "resposta")
-    @NotNull(message = "O enunciado é obrigatório.")
-    RespostaRequestDto resposta
+    @JsonProperty(value = "respostas")
+    List<RespostaRequestDto> respostas
 
 ) {
 }

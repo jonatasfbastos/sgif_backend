32276aa91bf2e6fabd1b1e49175174f1058db981
NekoYasha <autornekoyasha@gmail.com>
2023-10-13 22:07:58 -0300

    📜 feat: Criando enum com mensagens de exceções

--- src/main/java/br/com/ifba/infrastructure/exception/BusinessExceptionMessage.java
+++ src/main/java/br/com/ifba/infrastructure/exception/BusinessExceptionMessage.java
@@ -1,2 +1,16 @@
-package br.com.ifba.infrastructure.exception;public enum BusinessExceptionMessage {
+package br.com.ifba.infrastructure.exception;
+
+public enum BusinessExceptionMessage {
+
+    NOT_FOUND("A instância em questão não existe na base de dados");
+
+    private final String mensagem;
+
+    BusinessExceptionMessage(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }

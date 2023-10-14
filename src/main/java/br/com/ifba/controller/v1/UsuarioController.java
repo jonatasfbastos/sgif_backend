4d4d171004b6287cf84f88480142a3e5ed95cbc2
andesson reis <andessonreys@gmail.com>
2023-10-13 22:27:48 -0300

    feat: adiciona controle para getUsuario

--- src/main/java/br/com/ifba/controller/v1/UsuarioController.java
+++ src/main/java/br/com/ifba/controller/v1/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.validation.Valid;
+
+/**
+ * 
+ * 
+ * @author Andesson reis
+ */
 
 @RestController
 @RequestMapping(path = "/apif/v1")
@@ -35,6 +43,22 @@ public class UsuarioController {
         } catch (Exception e) {
             HashMap<String, String> erro = new HashMap<>();
             erro.put("error", "Não foi possível obter a lista de usuários.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+        }
+    }
+
+    @RequestMapping(path = "/usuario/{id}")
+    public ResponseEntity<?> getUsuario(@PathVariable Long id) {
+        try {
+            UsuarioResponseDto usuario = usuarioService.findById(id);
+            return ResponseEntity.ok(usuario);
+        } catch (NoSuchElementException e) {
+            HashMap<String, String> erro = new HashMap<>();
+            erro.put("error", "Usuário não encontrado");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+        } catch (Exception e) {
+            HashMap<String, String> erro = new HashMap<>();
+            erro.put("error", "Não foi possível obter o usuário.");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
         }
     }

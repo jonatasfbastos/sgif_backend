01c9d1cceb1c97a46aabd5f43a7b49a411ff5a5a
NekoYasha <autornekoyasha@gmail.com>
2023-10-13 21:41:14 -0300

    📜 feat: Criando endpoint par atualizar cadastros
    Principais mudanças:
    1. Criação do endpoint para atualizar os formulários.
    2. Criação do 'getResultErrors'.
    3. Adição de comentários.

--- src/main/java/br/com/ifba/controller/v1/FormularioController.java
+++ src/main/java/br/com/ifba/controller/v1/FormularioController.java
@@ -10,12 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+/**
+ * @author Giovane Neves
+ * Desde V1.0.1
+ */
 @RestController
 @RequestMapping(path = "/apif/v1")
 public class FormularioController {
@@ -32,27 +37,63 @@ public class FormularioController {
     // =========================================================== //
 
     /**
-     * @apiNote Endpoint criado desde a versão 1.0.0
+     * @author Giovane Neves
      *
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Salva um formulário.
      * @return uma entidade de resposta generica.
      */
     @PostMapping(path = "/formularios/formulario")
     public ResponseEntity<?> salvarFormulario(@RequestBody FormularioRequestDto formDto, BindingResult result){
 
-
         if(result.hasErrors()){
 
-            HashMap<String, String> erros = new HashMap<>();
-
-            for(FieldError erro : result.getFieldErrors())
-                erros.put(erro.getField(), erro.getDefaultMessage());
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getResultErrors(result));
 
         }
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(formularioService.salvarFormulario(Formulario.fromRequestDto(formDto)));
+    }
+
+    /**
+     * @author Giovane Neves
+     *
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Atualiza um formulário.
+     * @return uma entidade de resposta generica.
+     */
+    @PutMapping(path = "/formularios/formulario")
+    public ResponseEntity<?> atualizarFormulario(@RequestBody FormularioRequestDto formDto, BindingResult result){
+
+        if(result.hasErrors()){
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getResultErrors(result));
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(formularioService.atualizarFormulario(Formulario.fromRequestDto(formDto)));
+
+    }
+
+    /**
+     * Pega os erros de validações da requisição e guarda-os em um mapa.
+     *
+     * @param result - O objeto contendo os erros.
+     * @return um mapa com erros de validação da requisição.
+     */
+    private HashMap<String, String> getResultErrors(BindingResult result){
+
+        HashMap<String, String> erros = new HashMap<>();
+
+        for(FieldError erro : result.getFieldErrors())
+            erros.put(erro.getField(), erro.getDefaultMessage());
+
+        return erros;
+
     }
 
 }

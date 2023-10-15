d06411644882559daea284f87ab550ca3e7daabc
andesson reis <andessonreys@gmail.com>
2023-10-14 23:20:18 -0300

    feat: adicionado exception de 'login' ja existente no banco de dados

--- src/main/java/br/com/ifba/infrastructure/exception/BusinessExceptionMessage.java
+++ src/main/java/br/com/ifba/infrastructure/exception/BusinessExceptionMessage.java
@@ -2,7 +2,10 @@ package br.com.ifba.infrastructure.exception;
 
 public enum BusinessExceptionMessage {
 
-    NOT_FOUND("A instância em questão não existe na base de dados");
+    NOT_FOUND("A instância em questão não existe na base de dados"),
+
+    LOGIN_ALREADY_IN_USE("O login já está em uso");
+
 
     private final String mensagem;
 

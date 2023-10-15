3965b6cdae93a8baecc1131ca2ff8c64dd4cdafb
andesson reis <andessonreys@gmail.com>
2023-10-14 21:16:07 -0300

    Refactor: Atualização da Interface IUsuarioService para usar objetos DTO

--- src/main/java/br/com/ifba/entity/usuario/service/IUsuarioService.java
+++ src/main/java/br/com/ifba/entity/usuario/service/IUsuarioService.java
@@ -1,32 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.com.ifba.entity.usuario.service;
 
 import java.util.List;
 
 import br.com.ifba.entity.usuario.dto.UsuarioResponseDto;
 import br.com.ifba.entity.usuario.model.Usuario;
+
 import org.springframework.stereotype.Service;
 
 /**
  *
- * @author vitor
+ * @author Andesson Reis
+ * Desde V1.0.1
  */
 @Service
 public interface IUsuarioService {
 
-    public abstract Usuario saveUsuario(Usuario usuario);
+    /**
+     * Salva um Usuario na base de dados e retorna um objeto DTO com os dados resumidos do usuario salvo.
+     *
+     * @param usuario - O Usuario que será salvo na base de dados.
+     * @return um objeto DTO com os dados resumidos do usuario salvo.
+     */
+    UsuarioResponseDto saveUsuario(Usuario usuario);
 
-    public abstract void deleteUsuario(Usuario usuario);
-    
-    public abstract List<UsuarioResponseDto> getAllUsuariosDto();
+    /**
+     * Deleta um Usuario da base de dados.
+     *
+     * @param usuario - O Usuario que será deletado da base de dados.
+     */
+    void deleteUsuario(Usuario usuario);
 
-    //public abstract List<Usuario> getAllUsuarios();
+    /**
+     * Obtém uma lista de todos os usuários como objetos DTO.
+     *
+     * @return uma lista de objetos DTO representando os usuários.
+     */
+    List<UsuarioResponseDto> getAllUsuariosDto();
 
-    public Usuario findById(Long id);
+    /**
+     * Encontra um usuário pelo ID.
+     *
+     * @param id - O ID do usuário a ser encontrado.
+     * @return um objeto DTO com os dados resumidos do usuário encontrado.
+     */
+    UsuarioResponseDto findById(Long id);
 
-    public Usuario findByLoginAndSenha(String login, String senha);
-
+    /**
+     * Encontra um usuário pelo login e senha.
+     *
+     * @param login - O login do usuário.
+     * @param senha - A senha do usuário.
+     * @return um objeto DTO com os dados resumidos do usuário encontrado.
+     */
+    UsuarioResponseDto findByLoginAndSenha(String login, String senha);
 }

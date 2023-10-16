15c632719d8d9bedda26244ea332243e1fa91207
NekoYasha <autornekoyasha@gmail.com>
2023-10-16 10:14:52 -0300

    📜 feat: Criando endpoint para buscar formulários por ID

--- src/main/java/br/com/ifba/controller/v1/FormularioController.java
+++ src/main/java/br/com/ifba/controller/v1/FormularioController.java
@@ -9,7 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -30,6 +37,22 @@ public class FormularioController {
     // =========================================================== //
     // =============== [        ENDPOINTS       ] ================ //
     // =========================================================== //
+
+    /**
+     * @author Giovane Neves
+     * @apiNote  Endpoint criado desde a versão 1.0.1
+     *
+     * Procura um formulário na base de dados pelo ID passado
+     * como parâmetro da requisição.
+     * @return um entidade de resposta generica.
+     */
+    @GetMapping(path = "/formularios/formulario/{id}", consumes = "application/json")
+    public ResponseEntity<?> encontrarFormularioPorId(@PathVariable("id") Long id){
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(formularioService.encontrarFormularioPorId(id));
+
+    }
 
     /**
      * @author Giovane Neves

3fda09e4c3cce1a5ac7b758bf9ab6bcd09eaff54
andesson reis <andessonreys@gmail.com>
2023-10-16 15:24:32 -0300

    clean: Limpando referência aos métodos 'toDto' e 'fromDto'

--- src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
+++ src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
@@ -12,10 +12,7 @@ import br.com.ifba.entity.usuario.dto.UsuarioSimpleResponseDto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
-import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
 import br.com.ifba.entity.perfilusuario.model.PerfilUsuario;
-import br.com.ifba.entity.usuario.dto.UsuarioRequestDto;
-import br.com.ifba.entity.usuario.dto.UsuarioResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -55,45 +52,6 @@ public class Usuario extends PersistenceEntity {
     @JoinColumn(name = "perfil_usuario_id", referencedColumnName = "ID")
     @JsonIgnoreProperties("usuarios")
     private PerfilUsuario perfilUsuario;
-
-    /**
-     *
-     * @author Andesson Reis
-     *
-     *Fábrica de objeto Usuario.
-     *
-     * @param usuarioDto - O objeto de transferência com dados do Usario;
-     * @return um objeto Usuario.
-     */
-
-    public static Usuario fromRequestDto(UsuarioRequestDto usuarioDto) {
-
-        return new Usuario(
-                usuarioDto.login(),
-                usuarioDto.senha(),
-                new PerfilUsuario(
-                        usuarioDto.perfilUsuario().nome(),
-                        usuarioDto.perfilUsuario().descricao())
-                        );
-    }
-
-    /**
-     * @author Andesson Reis
-     *
-     * Cria um objeto de resposta DTO para ser enviado no ResponseBody
-     * das requisições direcionada à entidade 'Usuario'.
-     *
-     * @return um objeto UsuarioResponseDto com dados do formulário.
-     */
-
-     public UsuarioResponseDto toResponseDto() {
-        PerfilUsuarioResponseDto perfilDto = new PerfilUsuarioResponseDto(
-            this.login,  
-            this.password 
-        );
-    
-        return new UsuarioResponseDto(this.login, this.password, perfilDto);
-    }
 
     /**
      * @author Giovane Neves

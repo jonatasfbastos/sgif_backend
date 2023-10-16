bed3e4f875fbc01a02528115b3ceea8af7f98297
andesson reis <andessonreys@gmail.com>
2023-10-15 23:20:11 -0300

    refactor: mudança de parametros

--- src/main/java/br/com/ifba/entity/perfilusuario/service/IServicePerfilUsuario.java
+++ src/main/java/br/com/ifba/entity/perfilusuario/service/IServicePerfilUsuario.java
@@ -3,6 +3,7 @@ package br.com.ifba.entity.perfilusuario.service;
 import java.util.List;
 
 import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
+import br.com.ifba.entity.perfilusuario.model.PerfilUsuario;
 
 import org.springframework.stereotype.Service;
 
@@ -20,18 +21,18 @@ public interface IServicePerfilUsuario {
     /**
      * Salva um perfil de usuário (Response DTO) na base de dados.
      *
-     * @param perfilUsuarioResponseDTO O perfil de usuário (Response DTO) a ser salvo.
-     * @return O perfil de usuário (Response DTO) salvo.
+     * @param perfilUsuario O perfil de usuário (Response DTO) a ser salvo.
+     * @return O perfil de usuário salvo.
      */
-    PerfilUsuarioResponseDto savePerfilUsuario(PerfilUsuarioResponseDto perfilUsuarioResponseDTO);
+    PerfilUsuarioResponseDto savePerfilUsuario(PerfilUsuario perfilUsuario);
 
     /**
      * Atualiza um perfil de usuário (Response DTO) na base de dados.
      *
-     * @param perfilUsuarioResponseDTO O perfil de usuário (Response DTO) a ser atualizado.
+     * @param perfilUsuario O perfil de usuário a ser atualizado.
      * @return O perfil de usuário (Response DTO) atualizado.
      */
-    PerfilUsuarioResponseDto updatePerfilDeUsuario(PerfilUsuarioResponseDto perfilUsuarioResponseDTO);
+    PerfilUsuarioResponseDto updatePerfilDeUsuario(PerfilUsuario perfilUsuario);
 
     /**
      * Deleta um perfil de usuário (Response DTO) da base de dados.

315292578d94ce19a595741e1c4f8d92fafc09e7
andesson reis <andessonreys@gmail.com>
2023-10-16 11:23:57 -0300

    rafactor: atualizando metodo de atualizar perfil de usuario

--- src/main/java/br/com/ifba/entity/perfilusuario/service/ServicePerfilUsuario.java
+++ src/main/java/br/com/ifba/entity/perfilusuario/service/ServicePerfilUsuario.java
@@ -1,62 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.perfilusuario.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
+import br.com.ifba.infrastructure.support.StringUtil;
 import br.com.ifba.entity.perfilusuario.dao.IDaoPerfilUsuario;
+import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
 import br.com.ifba.entity.perfilusuario.model.PerfilUsuario;
+import br.com.ifba.entity.usuario.dto.UsuarioResponseDto;
+import br.com.ifba.entity.usuario.model.Usuario;
 
 /**
+ * Service que fornece operações relacionadas a perfis de usuário
+ * 
+ * Esta classe contém métodos para criar, atualizar, excluir e recuperar
+ * informações de perfis de usuários.
  *
- * @author Igor Lopes e Eduarda
+ * @author Andesson Reis
+ * @since V1.0.1
  */
 @Service
 public class ServicePerfilUsuario implements IServicePerfilUsuario {
+    // =========================================================== //
+    // =============== [ ATRIBUTOS ] ================ //
+    // =========================================================== //
 
-    // CONSTANTES
-
-    // mensagem de erro se o Usuario for null;
-    public final static String USUARIO_NULL = "Perfil de Usuário null";
-
-    // mensagem de erro se o Usuario já existir;
-    public final static String USUARIO_EXISTE = "O Perfil de Usuário já existe,";
-
-    // mensagem de erro se o Usuario não existir no banco;
-    public final static String USUARIO_NAO_EXISTE = "O Perfil de Usuário não existe na base de dados";
-
-    // mensagem de erro se o Usuario for inválido;
-    public final static String USUARIO_INVALIDO = "Perfil de Usuário inválido";
-
-    // OBJETO
     @Autowired
-    private IDaoPerfilUsuario daoPerfilDeUsuario;
+    private IDaoPerfilUsuario daoPerfilUsuario;
 
+    // =========================================================== //
+    // =============== [ MÉTODOS ] ================== //
+    // =========================================================== //
+
+    /**
+     * Salva um perfil de usuário (Response DTO) na base de dados.
+     *
+     * @param perfilUsuario perfil de usuário (Response DTO) a ser salvo.
+     * @return O perfil de usuário (Response DTO) salvo.
+     */
     @Override
-    public PerfilUsuario savePerfilUsuario(PerfilUsuario perfilDeUsuario) {
-        if (perfilDeUsuario == null) {
-            throw new BusinessException(USUARIO_NULL);
-        } else {
-            return daoPerfilDeUsuario.save(perfilDeUsuario);
-        }
+    public PerfilUsuarioResponseDto savePerfilUsuario(PerfilUsuario perfilUsuario) {
+        return daoPerfilDeUsuario.save()
     }
 
+    /**
+     * Atualiza um perfil de usuário (Response DTO) na base de dados.
+     *
+     * @param perfilUsuarioResponseDTO O perfil de usuário (Response DTO) a ser atualizado.
+     * @return O perfil de usuário (Response DTO) atualizado.
+     */
     @Override
-    public PerfilUsuario updatePerfilDeUsuario(PerfilUsuario perfilDeUsuario) {
-        if (perfilDeUsuario == null) {
-            throw new BusinessException(USUARIO_NULL);
-        } else if (daoPerfilDeUsuario.findById(perfilDeUsuario.getId()) == null) {
-            throw new BusinessException(USUARIO_NAO_EXISTE);
-        } else {
-            return daoPerfilDeUsuario.save(perfilDeUsuario);
-        }
+    public PerfilUsuarioResponseDto updatePerfilDeUsuario(PerfilUsuario perfilUsuario) {
+    
+        return daoPerfilUsuario.findById(perfilUsuario.getId())
+                .map(perfilUsuarioAtual -> {
+                    perfilUsuarioAtual.setNome(perfilUsuario.getNome());
+                    return daoPerfilUsuario.save(perfilUsuarioAtual).toResponseDto();
+                })
+                .orElseThrow(() -> new BusinessException(BusinessExceptionMessage.NOT_FOUND.getMensagem()));
     }
+    
 
     @Override
     public void deletePerfilUsuario(PerfilUsuario perfilDeUsuario) {
@@ -70,8 +80,9 @@ public class ServicePerfilUsuario implements IServicePerfilUsuario {
 
     @Override
     public List<PerfilUsuario> getAllPerfilUsuario() {
-        return (List<PerfilUsuario>)this.daoPerfilDeUsuario.findAll();
+        return (List<PerfilUsuario>) this.daoPerfilDeUsuario.findAll();
     }
+
 
     @Override
     public List<PerfilUsuario> findByNome(String name) {

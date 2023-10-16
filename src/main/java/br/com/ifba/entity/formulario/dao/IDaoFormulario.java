16c6acf57ffed7e8c6575125471437f58deb9e49
NekoYasha <autornekoyasha@gmail.com>
2023-10-16 10:15:57 -0300

    📜 feat: Adicionando método 'findById'

--- src/main/java/br/com/ifba/entity/formulario/dao/IDaoFormulario.java
+++ src/main/java/br/com/ifba/entity/formulario/dao/IDaoFormulario.java
@@ -1,6 +1,7 @@
 package br.com.ifba.entity.formulario.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.ifba.entity.formulario.model.Formulario;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface IDaoFormulario extends JpaRepository<Formulario, Long>{
-    
+
+    /**
+     * Encontra um formulário pelo ID passado por parâmetro.
+     *
+     * @param id O ID do formulário a ser enconotrado.
+     *
+     * @return um objeto opcional do tipo formulário,
+     * que será nulo caso o formulário não exista na base de dados.
+     */
+    Optional<Formulario> findById(Long id);
+
     List<Formulario> findByTitulo(String titulo);
     boolean existsByTitulo(String titulo);
     boolean existsById(Long id);

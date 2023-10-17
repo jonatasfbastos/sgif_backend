046818b590811ba62971d64c97cf05fca13ecce7
andesson reis <andessonreys@gmail.com>
2023-10-17 11:09:03 -0300

    Feat: Adiciona endpoint para deletar usuario por ID

--- src/main/java/br/com/ifba/controller/v1/UsuarioController.java
+++ src/main/java/br/com/ifba/controller/v1/UsuarioController.java
@@ -2,24 +2,21 @@ package br.com.ifba.controller.v1;
 
 import br.com.ifba.controller.v1.util.ResultError;
 import br.com.ifba.entity.usuario.dto.UsuarioRequestDto;
-import br.com.ifba.entity.usuario.dto.UsuarioResponseDto;
 import br.com.ifba.entity.usuario.model.Usuario;
 import br.com.ifba.entity.usuario.service.IUsuarioService;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 import javax.validation.Valid;
 
@@ -34,47 +31,49 @@ import javax.validation.Valid;
 @RequestMapping(path = "/apif/v1")
 public class UsuarioController {
 
+    // =========================================================== //
+    // =============== [ ATRIBUTOS ] ============================= //
+    // =========================================================== //
     @Autowired
     IUsuarioService usuarioService;
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [ ENDPOINTS ] ============================= //
+    // =========================================================== //
 
     /**
      * @apiNote Endpoint criado desde a versão 1.0.1
      *
-     *Obtém a lista de todos os usuários.
+     * Obtém a lista de todos os usuários.
      * 
      * @return Uma lista de usuários ou uma resposta de erro em caso de falha.
      * 
      * @author Andesson Reis
      */
-    @RequestMapping(path = "/usuarios", produces = "application/json")
+    @GetMapping(path = "/usuarios", consumes = "application/json")
     public ResponseEntity<?> getUsuarios() {
-        try {
-            List<UsuarioResponseDto> usuarios = usuarioService.getAllUsuarios();
-            return ResponseEntity.ok(usuarios);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResultError.getResultErrors(null));
-        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(usuarioService.getAllUsuarios());
     }
 
-     /**
+    /**
      * @apiNote Endpoint criado desde a versão 1.0.1
      *
-     *Obtém um usuário.
-     * 
+     * Obtém um usuário.
+     *
      * @return Um usuário ou uma resposta de erro em caso de falha.
-     * 
+     *
      * @author Andesson Reis
      */
-    @RequestMapping(path = "/usuario/{id}")
-    public ResponseEntity<UsuarioResponseDto> getUsuario(@Valid @PathVariable Long id) {
-        try {
-            UsuarioResponseDto usuario = usuarioService.findById(id);
-            return ResponseEntity.ok(usuario);
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+    @GetMapping(path = "/usuario/{id}", consumes = "application/json")
+    public ResponseEntity<?> getUsuario(@PathVariable("id") Long id) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(usuarioService.findById(id));
     }
 
     /**
@@ -92,8 +91,22 @@ public class UsuarioController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED)
-                        .body(usuarioService.saveUsuario(Usuario.fromRequestDto(usuarioDto)));
+                        .body(usuarioService.saveUsuario(objectMapperUtil.map(usuarioDto, Usuario.class)));
 
+    }
+
+    /**
+     * @author Andesson Reis
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Deleta um usuario.
+     * @return uma entidade de resposta generica.
+     */
+    @DeleteMapping(path = "/usuarios/usuario/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarUsuarioPorID(@PathVariable("id") Long id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(usuarioService.deleteUsuario(id));
     }
 
 }

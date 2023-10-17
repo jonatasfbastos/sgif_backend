38992582e7b94e4b3248450a3c5e7cfd9ff8e1b1
NekoYasha <autornekoyasha@gmail.com>
2023-10-17 09:55:27 -0300

    🎨 style: Adiconando comentários padronizados.

--- src/main/java/br/com/ifba/entity/curso/model/Curso.java
+++ src/main/java/br/com/ifba/entity/curso/model/Curso.java
@@ -3,6 +3,7 @@ package br.com.ifba.entity.curso.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
@@ -15,24 +16,68 @@ import br.com.ifba.entity.modalidade.model.Modalidade;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity
-@Table(name = "curso")
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * Desde V1.0.1
+ *
+ * Classe que representa um curso da instituição.
+ * Veja também: {@link br.com.ifba.entity.modalidade.model.Modalidade}
+ * Veja também: {@link br.com.ifba.entity.matrizcurricular.model.MatrizCurricular}
+ */
+@Entity(name = "curso")
+@Table(name = "cursos")
 @Data
 @EqualsAndHashCode(callSuper = false)
-
 public class Curso extends PersistenceEntity implements Serializable{
-    
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+
+    /**
+     * O nome do curso, não pode ser nulo.
+     */
+    @Column(name = "nome", nullable = false)
     private String nome;
+
+    /**
+     * O código do curso, deve ser único e não
+     * pode ser nulo.
+     */
+    @Column(name = "codigo_curso", nullable = false, unique = true)
     private String codigoCurso;
+
+    /**
+     * A sigla do curso.
+     * Exemplos: 'ADS', 'MI', 'INFO'.
+     */
+    @Column(name = "sigla")
     private String sigla;
+
+    /**
+     * A descrição do curso.
+     */
+    @Column(name = "descricao")
     private String descricao;
+
+    /**
+     * O status do curso, é 'false' por padrão.
+     */
+    @Column(name = "ativo")
     boolean ativo;
-    
-    //Varios cursos podem estar associados a uma modalidade
+
+    /**
+     * A modalidade do curso, cada curso só pode estar associado a uma modalidade.
+     */
     @ManyToOne(fetch = FetchType.EAGER)    
     private Modalidade modalidade;
-    
-    //Um curso esta associado a uma ou mais matrizes curriculares
+
+    /**
+     * A matriz curricular do curso.
+     * Um curso pode estar associado a uma ou mais matrizes curriculares.
+     */
     @OneToMany(mappedBy = "curso", fetch = FetchType.LAZY)
     private List <MatrizCurricular> matrizCurricular;
+
 }

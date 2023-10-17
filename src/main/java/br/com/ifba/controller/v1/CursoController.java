42a860370049d817359f2647d06c6a23e21d3cc6
NekoYasha <autornekoyasha@gmail.com>
2023-10-17 10:04:00 -0300

    📜 feat: Criando contoller para entidade 'curso'

--- src/main/java/br/com/ifba/controller/v1/CursoController.java
+++ src/main/java/br/com/ifba/controller/v1/CursoController.java
@@ -1,2 +1,26 @@
-package br.com.ifba.controller.v1;public class CursoController {
+package br.com.ifba.controller.v1;
+
+import br.com.ifba.entity.curso.service.ICursoService;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Giovane Neves
+ * Desde V1.0.1
+ */
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class CursoController {
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+    @Autowired
+    ICursoService cursoService;
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
 }

b6144b4f81cba0061c7bdf57e1086b9be1bd0a83
andesson reis <andessonreys@gmail.com>
2023-10-18 17:57:03 -0300

    feat: Criado controller para 'PerfilUsuario'

---
+++ src/main/java/br/com/ifba/controller/v1/PerfilUsuarioController.java
@@ -0,0 +1,37 @@
+package br.com.ifba.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifba.entity.perfilusuario.service.IServicePerfilUsuario;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+
+/**
+ * 
+ * 
+ * @author Andesson reis
+ * Desde V1.0.1
+ */
+
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class PerfilUsuarioController {
+
+    // =========================================================== //
+    // =============== [ ATRIBUTOS ] ============================= //
+    // =========================================================== //
+    @Autowired
+    IServicePerfilUsuario perfilUsuarioService;
+   
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [ ENDPOINTS ] ============================= //
+    // =========================================================== //
+
+    
+    
+}

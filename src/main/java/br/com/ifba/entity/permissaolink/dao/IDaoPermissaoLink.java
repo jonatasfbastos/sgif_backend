2b5fa950d5e2ff2c9b7b1939e268a6b48ee81fce
andesson reis <andessonreys@gmail.com>
2023-10-19 17:23:01 -0300

    refactor: Mudando id de 'Long' para 'UUID'

--- src/main/java/br/com/ifba/entity/permissaolink/dao/IDaoPermissaoLink.java
+++ src/main/java/br/com/ifba/entity/permissaolink/dao/IDaoPermissaoLink.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface IDaoPermissaoLink extends JpaRepository<PermissaoLink, Long> {
 
     boolean existsByUrl(String url);
-    List<PermissaoLink> findByPermissoesId(Long id);
+    List<PermissaoLink> findByPermissoesId(UUID id);
 
 }

b04a9286cc314c33b4f42007a318abb2a5c8b374
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 13:40:58 -0300

    📜 feat: Criando DTO para requisições direcionadas à entidade  curso

--- src/main/java/br/com/ifba/entity/curso/dto/CursoRequestDto.java
+++ src/main/java/br/com/ifba/entity/curso/dto/CursoRequestDto.java
@@ -1,2 +1,20 @@
-package br.com.ifba.entity.curso.dto;public record CursoRequestDto() {
+package br.com.ifba.entity.curso.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public record CursoRequestDto(
+
+        @JsonProperty(value = "nome")
+        @NotNull(message = "O nome do curso é obrigatório")
+        @NotNull(message = "O nome do curso não pode ser vazio")
+        @Size(min = 3, message = "O nome do curso precisa ter mais de 3 caracteres")
+        String nome,
+        @JsonProperty(value = "codigo_curso")
+        @NotNull(message = "O código do curso é obrigatório")
+        @NotNull(message = "O código do curso não pode ser vazio")
+        String codigoCurso
+) {
 }

7cd826dd3a13bbc7f058d805c433f27ae3517cb6
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 10:30:54 -0300

    📜 feat: Criado objeto de resposta DTO para 'MatrizCurricular'

--- src/main/java/br/com/ifba/entity/matrizcurricular/dto/MatrizCurricularResponseDto.java
+++ src/main/java/br/com/ifba/entity/matrizcurricular/dto/MatrizCurricularResponseDto.java
@@ -1,2 +1,24 @@
-package br.com.ifba.entity.matrizcurricular.dto;public record MatrizCurricularResponseDto() {
+package br.com.ifba.entity.matrizcurricular.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Giovane Neves
+ *
+ * DTO para respostas de requisições direcionadas
+ * à entidade 'MatrizCurricular'.
+ * 
+ * @param nome O nome da matriz curricular.
+ * @param descricao A descrição da matriz curricular.
+ *
+ * Veja também: {@link br.com.ifba.entity.matrizcurricular.model.MatrizCurricular}
+ */
+public record MatrizCurricularResponseDto(
+        @JsonProperty(value = "nome")
+        String nome,
+        @JsonProperty(value = "descricao")
+        String descricao
+
+        // TODO: Adicionar (e criar, caso não exista) o DTO 'EtapaCursoResponseDTO'.
+) {
 }

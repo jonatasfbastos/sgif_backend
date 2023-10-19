08c9d561c7f4538bf3e65dbcd461bf78307fa4e0
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 10:24:31 -0300

    📜 feat: Adiocionando assinatura de método 'encontrar por id'

--- src/main/java/br/com/ifba/entity/curso/dao/IDaoCurso.java
+++ src/main/java/br/com/ifba/entity/curso/dao/IDaoCurso.java
@@ -4,8 +4,26 @@ import br.com.ifba.entity.curso.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * Desde V1.0.1
+ */
 public interface IDaoCurso extends JpaRepository<Curso, Long>{
-    
+
+    /**
+     * @author Giovane Neves
+     * Desde V1.0.1
+     *
+     * Encontra um formulário pelo ID passado por parâmetro.
+     * @param id O ID do formulário a ser enconotrado.
+     * @return um objeto opcional do tipo formulário,
+     * que será nulo caso o formulário não exista na base de dados.
+     */
+    Optional<Curso> findById(UUID id);
+
     List <Curso> findByNome (String nome);
 }

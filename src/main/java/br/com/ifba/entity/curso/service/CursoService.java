d1c57c95d105d153a50d5e091b2af13adcb56eea
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 10:33:10 -0300

    ✏ refactor: Editando método 'listar' e 'encontrar por id'

--- src/main/java/br/com/ifba/entity/curso/service/CursoService.java
+++ src/main/java/br/com/ifba/entity/curso/service/CursoService.java
@@ -1,18 +1,29 @@
 package br.com.ifba.entity.curso.service;
 
 import java.util.List;
+import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import br.com.ifba.entity.curso.dto.CursoResponseDto;
 import br.com.ifba.entity.curso.dao.IDaoCurso;
 import br.com.ifba.entity.curso.model.Curso;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 import br.com.ifba.infrastructure.exception.BusinessException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * Desde V1.0.1
+ */
 @Service
 public class CursoService implements ICursoService {
-    
-    //-_-_-_-_-_-_-_-_-_- CONSTANTES -_-_-_-_-_-_-_-_-_-
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
     
     //mensagem de erro caso o Curso seja nulo;
     public final static String CURSO_NULL = "Dados do Curso nao preenchidos";
@@ -22,22 +33,60 @@ public class CursoService implements ICursoService {
     
     //mensagem de erro caso o Curso não exista no banco de dados;
     public final static String CURSO_NAO_EXISTE = "Curso nao existente no Banco de dados";
-    
-    //mensagem de erro caso o Curso seja inválido;
-    public final static String CURSO_INVALIDO = "As informaacoes do Curso nao sao validas";
-    
+
     // Mensagem de erro se o Curso possuir matriz curricular
     public final static String CURSO_POSSUI_MATRIZ_CURRICULAR = "Curso possui matriz curricular";
-    
-    //-_-_-_-_-_-_-_-_-_- OBJETO -_-_-_-_-_-_-_-_-_-
+
     
     @Autowired
      private IDaoCurso cursoDao;
-     
-    //-_-_-_-_-_-_-_-_-_- MÉTODOS -_-_-_-_-_-_-_-_-_-
-    
+
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [        MÉTODOS       ] ================== //
+    // =========================================================== //
+
+    /**
+     * @author Giovane Neves
+     * Desde V1.0.1
+     *
+     * Lista todos os cursos cadastrados na base de dados.
+     * @return uma lista com todos os cursos da base de dados.
+     */
     @Override
-    public Curso saveCurso(Curso curso) {
+    public List<CursoResponseDto> listarCursos() {
+
+        return objectMapperUtil.mapAll(
+                this.cursoDao.findAll(),
+                CursoResponseDto.class
+        );
+    }
+
+    /**
+     * @author Giovane Neves
+     * Desde V1.0.1
+     *
+     * Busca o curso cadastrado na base de dados atrelado ao ID
+     * passado por parâmetro.
+     * @param id O ID do curso a ser buscado.
+     * @return o curso atrelado ao ID passado por parâmetro.
+     */
+    @Override
+    public CursoResponseDto encontrarCursoPorId(UUID id) {
+
+
+        return objectMapperUtil.map(
+                this.cursoDao.findById(id)
+                        .orElseThrow(() -> new BusinessException(BusinessExceptionMessage.NOT_FOUND.getMensagem())),
+                CursoResponseDto.class
+        );
+
+    }
+
+    @Override
+    public Curso salvarCurso(Curso curso) {
         if(curso == null){
             throw new BusinessException(CURSO_NULL);
         } else if(cursoDao.existsById(curso.getId()) == true){
@@ -48,46 +97,32 @@ public class CursoService implements ICursoService {
     }
 
     @Override
-    public Curso updateCurso(Curso curso) {
+    public Curso atualizarCurso(Curso curso) {
         if(curso == null){
             throw new BusinessException(CURSO_NULL);
         } else if(cursoDao.existsById(curso.getId()) == false) {
             throw new BusinessException(CURSO_NAO_EXISTE);
         } else {
             return cursoDao.save(curso);
-        }    
+        }
     }
 
     @Override
-    public void deleteCurso(Curso curso) {
+    public void deletarCurso(Curso curso) {
         if(curso == null){
             throw new BusinessException(CURSO_NULL);
         }else if(this.cursoDao.existsById(curso.getId()) == true) {
-            if (curso.getMatrizCurricular() == null){
+            if (curso.getMatrizesCurriculares() == null){
                 throw new BusinessException(CURSO_POSSUI_MATRIZ_CURRICULAR);
             }
             this.cursoDao.delete(curso);
             return;
         }
-            throw new BusinessException(CURSO_NAO_EXISTE);    
-            
+            throw new BusinessException(CURSO_NAO_EXISTE);
+
 }
 
-    @Override
-    public List<Curso> getAllCurso() {
-        return this.cursoDao.findAll();    
-    }
 
-    @Override
-    public Curso findById(Long id) {
-        if(id == null) {
-            throw new BusinessException("id null");
-        } else if(id <= 0) {
-            throw new BusinessException("id invalido");
-        } else {
-            return cursoDao.findById(id).orElse(null);
-        }
-    }
 }
 
     
--- src/main/java/br/com/ifba/entity/curso/service/ICursoService.java
+++ src/main/java/br/com/ifba/entity/curso/service/ICursoService.java
@@ -1,26 +1,47 @@
 package br.com.ifba.entity.curso.service;
 
 import java.util.List;
+import java.util.UUID;
 
+import br.com.ifba.entity.curso.dto.CursoResponseDto;
 import org.springframework.stereotype.Service;
 
 import br.com.ifba.entity.curso.model.Curso;
 
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * Desde V1.0.1
+ */
 @Service
 public interface ICursoService {
-    
-    public abstract Curso saveCurso(Curso curso);
-    
+
+    /**
+     * @author Giovane Neves
+     * Desde V1.0.1
+     *
+     * Lista todos os cursos cadastrados na base de dados.
+     * @return uma lista com todos os cursos da base de dados.
+     */
+    List<CursoResponseDto> listarCursos();
+
+    /**
+     * @author Giovane Neves
+     * Desde V1.0.1
+     *
+     * Busca o curso cadastrado na base de dados atrelado ao ID
+     * passado por parâmetro.
+     * @param id O ID do curso a ser buscado.
+     * @return o curso atrelado ao ID passado por parâmetro.
+     */
+    CursoResponseDto encontrarCursoPorId(UUID id);
+
+    Curso salvarCurso(Curso curso);
+
     //atualiza Curso
-    public abstract Curso updateCurso(Curso curso);
-    
+    Curso atualizarCurso(Curso curso);
+
     //deleta Curso
-    public abstract void deleteCurso(Curso curso);
-    
-    // Metodo que retorna todos os Cursos da base de dados
-    public abstract List<Curso> getAllCurso();
-    
-    // Metodo que procura curso pelo id
-    public Curso findById (Long id);
-    
+    void deletarCurso(Curso curso);
+
 }

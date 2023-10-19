64d26be96c7611cac6635cc3f9ade3bbe7dfb39f
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 13:38:56 -0300

    📜 feat: Criando endpoint 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/CursoController.java
+++ src/main/java/br/com/ifba/controller/v1/CursoController.java
@@ -1,5 +1,8 @@
 package br.com.ifba.controller.v1;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.curso.dto.CursoRequestDto;
+import br.com.ifba.entity.curso.model.Curso;
 import br.com.ifba.entity.curso.service.ICursoService;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -8,11 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
@@ -66,6 +68,51 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(cursoService.encontrarCursoPorId(id));
 
+    }
+
+    /**
+     * @author Giovane Neves
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Salva um curso na base de dados.
+     * @return uma entidade de resposta generica.
+     */
+    @PostMapping(path = "/cursos/curso", consumes = "application/json")
+    public ResponseEntity<?> salvarCurso(@Valid @RequestBody CursoRequestDto cursoDto, BindingResult result){
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(cursoService.salvarCurso(objectMapperUtil.map(cursoDto, Curso.class)));
+    }
+
+    /**
+     * @author Giovane Neves
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Atualiza um curso.
+     * @return uma entidade de resposta generica.
+     */
+    @PutMapping(path = "/cursos/curso", consumes = "application/json")
+    public ResponseEntity<?> atualizarCurso(@Valid @RequestBody CursoRequestDto cursoDto, BindingResult result){
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(cursoService.atualizarCurso(objectMapperUtil.map(cursoDto, Curso.class)));
+
+    }
+
+    /**
+     * @author Giovane Neves
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Deleta o curso atrelado ao ID passado por parâmetro.
+     * @return uma entidade de resposta generica.
+     */
+    @DeleteMapping(path = "/cursos/curso/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarCursoPorId(@PathVariable("id") @NotNull UUID id){
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(cursoService.deletarCursoPorId(id));
     }
 
 }

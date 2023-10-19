707646a1d3ae72749bddcb84773d6d7278058382
andesson reis <andessonreys@gmail.com>
2023-10-18 23:14:46 -0300

    refactor: refatorando interface do service para retornar um objeto DTO

--- src/main/java/br/com/ifba/entity/permissao/service/IServicePermissao.java
+++ src/main/java/br/com/ifba/entity/permissao/service/IServicePermissao.java
@@ -1,16 +1,62 @@
 package br.com.ifba.entity.permissao.service;
 
+import br.com.ifba.entity.permissao.dto.PermissaoResponseDto;
 import br.com.ifba.entity.permissao.model.Permissao;
 
 import java.util.List;
 
+/**
+ *
+ * @author Andesson Reis
+ * Desde V1.0.1
+ */
 public interface IServicePermissao {
+    /**
+     * Salva uma Permissão na base de dados e retorna um objeto DTO
+     *
+     *
+     * @param permissao - A Permissão que será salvo na base de dados.
+     * @return um objeto DTO com os dados resumidos da permissão salva.
+     */
+    PermissaoResponseDto savePermissao(Permissao permissao);
 
-    Permissao savePermissao(Permissao permissao);
-    String deletePermissao(Long id);
-    Permissao updatePermissao(Permissao permissao);
-    List<Permissao> getAllPermissoes();
+    /**
+     * Deleta uma Permissão.
+     *
+     * @param id O ID da permissão a ser deletado.
+     * @return objeto DTO com os dados da permissão deletado.
+     */
+    PermissaoResponseDto deletePermissao(Long id);
+
+    /**
+     * Atualiza uma permissão na base de dados.
+     *
+     * @param permissao A permissão a ser atualizado.
+     * @return A permissão atualizado.
+     */
+    PermissaoResponseDto updatePermissao(Permissao permissao);
+
+    /**
+     * Obtém uma lista de todas as permissão como objetos DTO.
+     *
+     * @return uma lista de objetos DTO representando as permissões.
+     */
+    List<PermissaoResponseDto> getAllPermissoes();
+
+    /**
+     * Encontra permissões por ID.
+     *
+     * @param id O id do perfil de usuario a ser pesquisado.
+     * @return Uma lista de permissões atrelado ao perfil.
+     */
     List<Permissao> getAllByPerfilId(Long id);
+
+    /**
+     * 
+     *
+     * @param id
+     * @return
+     */
     List<Permissao> getAllByLinkId(Long id);
 
 }

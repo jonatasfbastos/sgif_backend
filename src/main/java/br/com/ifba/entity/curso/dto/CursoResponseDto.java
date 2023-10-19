b13bcc8cb52b3b7087d77ac53956a574765f6872
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 10:29:18 -0300

    📜 feat: Criado objeto de resposta DTO para 'Curso'

--- src/main/java/br/com/ifba/entity/curso/dto/CursoResponseDto.java
+++ src/main/java/br/com/ifba/entity/curso/dto/CursoResponseDto.java
@@ -1,2 +1,44 @@
-package br.com.ifba.entity.curso.dto;public record CursoResponseDto() {
+package br.com.ifba.entity.curso.dto;
+
+import br.com.ifba.entity.matrizcurricular.dto.MatrizCurricularResponseDto;
+import br.com.ifba.entity.modalidade.dto.ModalidadeResponseDto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/**
+ * @author Giovane Neves
+ * Desde V1.0.1
+ *
+ * DTO para respostas de requisições direcionadas
+ * à entidade 'Curso'.
+ *
+ * @param nome O nome do curso.
+ * @param codigo_curso O código do curso.
+ * @param sigla A sigla do curso.
+ * @param descricao A descrição do curso.
+ * @param ativo O status do curso (ativo, true, ou desativado, false).
+ * @param modalidade A modalidade do curso.
+ * @param matrizesCurriculares As matrizes curriculares do curso, pode ter uma ou várias.
+ *
+ * Veja também: {@link br.com.ifba.entity.modalidade.dto.ModalidadeResponseDto}
+ * Veja também: {@link br.com.ifba.entity.matrizcurricular.dto.MatrizCurricularResponseDto}
+ */
+public record CursoResponseDto(
+        @JsonProperty(value = "nome")
+        String nome,
+        @JsonProperty(value = "codigo_curso")
+        String codigo_curso,
+        @JsonProperty(value = "sigla")
+        String sigla,
+        @JsonProperty(value = "descricao")
+        String descricao,
+        @JsonProperty(value = "ativo")
+        boolean ativo,
+        @JsonProperty(value = "modalidade")
+        ModalidadeResponseDto modalidade,
+        @JsonProperty(value = "matrizes_curriculares")
+        List<MatrizCurricularResponseDto> matrizesCurriculares
+) {
 }

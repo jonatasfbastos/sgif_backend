beea0f874fe6517b3e0bc21b3c66d25770061f13
andesson reis <andessonreys@gmail.com>
2023-10-19 19:12:33 -0300

    refactor: Atualizando metodo de salvar

--- src/main/java/br/com/ifba/entity/permissaolink/service/ServicePermissaoLink.java
+++ src/main/java/br/com/ifba/entity/permissaolink/service/ServicePermissaoLink.java
@@ -1,24 +1,40 @@
 package br.com.ifba.entity.permissaolink.service;
 
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
 import br.com.ifba.infrastructure.support.StringUtil;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 import br.com.ifba.entity.permissaolink.dao.IDaoPermissaoLink;
+import br.com.ifba.entity.permissaolink.dto.PermissaoLinkResponseDto;
 import br.com.ifba.entity.permissaolink.model.PermissaoLink;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+import javax.validation.Valid;
+
+/**
+ * @author Andesson Reis
+ *         Desde V1.0.1
+ */
 @Service
 public class ServicePermissaoLink implements IServicePermissaoLink {
 
-    private final static String CAMPO_VAZIO = "Há campo vazio, preencha e tente novamente.";
-    private final static String LINK_NAO_EXISTE = "O link informado não existe.";
-    private final static String LINK_EXISTE = "O link informado já existe.";
-    private final static String LINK_DELETADO = "Link deletado com sucesso.";
-    private final static String HA_PERMISSAO_ASSOCIADA = "Não foi possível deletar o link, há permissão associada a este link.";
+    // =========================================================== //
+    // ====================== [ ATRIBUTOS ] ====================== //
+    // =========================================================== //
 
     @Autowired
     private IDaoPermissaoLink daoLink;
+
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // ====================== [ MÉTODOS ] ======================== //
+    // =========================================================== //
 
     private boolean validarCampos(PermissaoLink permissaoLink) {
         if (StringUtil.getInstance().isNullOrEmpty(permissaoLink.getNome())) {
@@ -27,15 +43,23 @@ public class ServicePermissaoLink implements IServicePermissaoLink {
         return StringUtil.getInstance().isNullOrEmpty(permissaoLink.getUrl());
     }
 
+    /**
+     * Salva um PermissaoLink na base de dados e retorna um objeto DTO com os dados
+     * resumidos do PermissaoLink salvo.
+     *
+     * @param permissaoLink - O PermissaoLink que será salvo na base de dados.
+     * @return um objeto DTO com os dados resumidos do PermissaoLink salvo.
+     */
     @Override
-    public PermissaoLink saveLink(PermissaoLink permissaoLink) {
-        if (validarCampos(permissaoLink)) {
-            throw new BusinessException(CAMPO_VAZIO);
-        }
+    public PermissaoLinkResponseDto saveLink(@Valid PermissaoLink permissaoLink) {
+        // Verifica se já existe uma PermissaoLink com a mesma URL na base de dados
         if (daoLink.existsByUrl(permissaoLink.getUrl())) {
-            throw new BusinessException(LINK_EXISTE);
+            throw new BusinessException(BusinessExceptionMessage.ATTRIBUTE_VALUE_ALREADY_EXISTS.getMensagem());
         }
-        return daoLink.save(permissaoLink);
+
+        return objectMapperUtil.map(
+                daoLink.save(permissaoLink),
+                PermissaoLinkResponseDto.class);
     }
 
     @Override

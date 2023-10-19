ef57dc92006c76d320e426a81bd974a8d81cd658
NekoYasha <autornekoyasha@gmail.com>
2023-10-19 10:30:12 -0300

    📜 feat: Criado objeto de resposta DTO para 'Modalidade'

--- src/main/java/br/com/ifba/entity/modalidade/dto/ModalidadeResponseDto.java
+++ src/main/java/br/com/ifba/entity/modalidade/dto/ModalidadeResponseDto.java
@@ -1,2 +1,23 @@
-package br.com.ifba.entity.modalidade.dto;public record ModalidadeResponseDto() {
+package br.com.ifba.entity.modalidade.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Giovane Neves
+ * Desde V1.0.1
+ *
+ * DTO para retornar dados em respostas
+ * de requisições direcionadas à entidade 'Modalidade'.
+ *
+ * @param nome O nome da modalidade.
+ * @param descricao A descrição da modalidade.
+ *
+ * Veja também: {@link br.com.ifba.entity.modalidade.model.Modalidade}
+ */
+public record ModalidadeResponseDto(
+        @JsonProperty(value = "nome")
+        String nome,
+        @JsonProperty(value = "descricao")
+        String descricao
+) {
 }

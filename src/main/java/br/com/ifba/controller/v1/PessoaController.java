e88745feddb40fda893915d7635e777a4f379394
andesson reis <andessonreys@gmail.com>
2023-10-20 15:08:46 -0300

    feat: Criando endpoint para listar pessoas por nome

--- src/main/java/br/com/ifba/controller/v1/PessoaController.java
+++ src/main/java/br/com/ifba/controller/v1/PessoaController.java
@@ -1,9 +1,12 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +47,23 @@ public class PessoaController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(pessoaService.getAllPessoas());
+    }
+
+      /**
+     * @author Andesson Reis
+     * Desde V1.0.1
+     * 
+     * Encontra Pessoas pelo nome.
+     *
+     * @param name O nome a ser pesquisado.
+     * @return Uma lista de Pessoas com o nome especificado ou uma resposta de erro em caso de falha
+     *        
+     */
+    @GetMapping(path = "/pessoas/pessoas", consumes = "application/json")
+    public ResponseEntity<?> getPessoasPorNome(@Valid @PathVariable String nome) {
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pessoaService.findByNome(nome));
+   
     }
 }

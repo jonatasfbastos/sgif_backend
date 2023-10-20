ed22c0a28867229601a5816d78d35288ec158ffe
andesson reis <andessonreys@gmail.com>
2023-10-19 23:58:10 -0300

    feat: Criado endpoint para 'GetAllById' links

--- src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
+++ src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
@@ -1,9 +1,15 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.ifba.entity.permissaolink.service.IServicePermissaoLink;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
@@ -40,4 +46,21 @@ public class PermissaoLinkController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(permissaoLinkService.getAllLinks());
     }
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Obtém a lista de todas as permissõesLinks por ID.
+     *
+     * @return Uma lista de permissõesLinks ou uma resposta de erro em caso de falha.
+     *
+     * @author Andesson Reis
+     */
+    @GetMapping(path = "/listarLinksPermissao/{id}", consumes = "application/json")
+    public ResponseEntity<?> listarPermissoesPorId(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(permissaoLinkService.getAllByPermissaoId(id));
+    }
+
 }

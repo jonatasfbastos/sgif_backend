838e9287ece43554ff11ee3cec28b89450d0f2ee
NekoYasha <autornekoyasha@gmail.com>
2023-10-20 13:43:43 -0300

    ✏ refactor: Reescrevendo métodos do service

--- src/main/java/br/com/ifba/entity/aluno/service/AlunoService.java
+++ src/main/java/br/com/ifba/entity/aluno/service/AlunoService.java
@@ -1,57 +1,69 @@
 package br.com.ifba.entity.aluno.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import br.com.ifba.entity.aluno.dao.IDaoAluno;
 import br.com.ifba.entity.aluno.dto.AlunoResponseDto;
 import br.com.ifba.entity.aluno.model.Aluno;
+import br.com.ifba.entity.pessoa.dao.IDaoPessoa;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+import br.com.ifba.infrastructure.exception.BusinessException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.ifba.infrastructure.exception.BusinessException;
 
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * Desde V1.0.1
+ */
 @Service
-public class ServiceAluno implements IServiceAluno{
+public class AlunoService implements IAlunoService {
     
 
     // =========================================================== //
     // =============== [       ATRIBUTOS        ] ================ //
     // =========================================================== //
 
-    /**
-     * Mensagem de erro se o aluno for nulo.
-     */
-    public final static String ALUNO_NULL = "Dados do Aluno nao preenchidos";
-
-    /**
-     * Mensagem de erro para caso o aluno já exista na base de dados.
-     */
-    public final static String ALUNO_EXISTE = "Aluno ja existente no Banco de dados";
-
-    /**
-     * Mensagem de erro para caso o aluno não exista na base de dados.
-     */
-    public final static String ALUNO_NAO_EXISTE = "Aluno não existente no Banco de dados";
-
-    /**
-     * Mensagem de erro para caso o aluno tenha informações inválidas.
-     */
-    public final static String ALUNO_INVALIDO = "As informações do Aluno não são válidas";
-
-    /**
-     * Reposítório da entidade 'aluno'.
-     */
     @Autowired
     private IDaoAluno alunoDao;
+
+    @Autowired
+    private IDaoPessoa pessoaDao;
+
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
 
     // =========================================================== //
     // =============== [       MÉTODOS          ] ================ //
     // =========================================================== //
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * Lista todos os alunos presentes na base de dados.
+     * @return uma lista com todos os alunos da base de dados, ou nulo caso não exista nenhuma.
+     */
+    @Override
+    public List<AlunoResponseDto> listarAlunos() {
+
+        return objectMapperUtil.mapAll(
+                this.alunoDao.findAll(),
+                AlunoResponseDto.class
+        );
+
+    }
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Salva um aluno na base de ddados.
-     *
      * @param aluno - O Aluno que será salvo na base de dados.
      * @return um objeto DTO com os dados do aluno salvo na base de dados,
      * caso a operação ocorra com sucesso.
@@ -60,61 +72,63 @@ public class ServiceAluno implements IServiceAluno{
     @Override
     public AlunoResponseDto salvarAluno(Aluno aluno) {
 
-       if(aluno == null) {
-            throw new BusinessException(ALUNO_NULL);
-       } else{
-            return alunoDao.save(aluno).toResponseDto();
-       }
+        boolean existsByMatricula = this.alunoDao.existsByMatricula(aluno.getMatricula());
+        return Optional.of(aluno)
+                .filter(al -> !existsByMatricula)
+                .filter(al -> !this.pessoaDao.existsByCpf(al.getCpf()))
+                .map(al -> objectMapperUtil.map(this.alunoDao.save(al), AlunoResponseDto.class))
+                .orElseThrow(
+                        () -> new BusinessException(BusinessExceptionMessage.ATTRIBUTE_VALUE_ALREADY_EXISTS.getMensagemValorJaExiste(existsByMatricula ? "matrícula" : "CPF"))
+                );
 
     }
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Atualiza os dados de uma aluno existente na base de dados.
-     *
      * @param aluno - O Aluno que terá os seus dados atualizados.
      * @return os dados atualizados do aluno.
-     *
      */
     @Override
-    public Aluno atualizarAluno(Aluno aluno) {
-        if(aluno == null) {
-            throw new BusinessException(ALUNO_NULL);
-        } 
-        if(alunoDao.existsById(aluno.getId()) == false) {
-            throw new BusinessException(ALUNO_NAO_EXISTE);
-        }
-        return alunoDao.save(aluno);
+    public AlunoResponseDto atualizarAluno(Aluno aluno) {
+
+        boolean alunoExists = this.alunoDao.findById(aluno.getId()).isPresent();
+
+        return Optional.of(aluno)
+                .filter(al -> alunoExists)
+                .filter(al -> this.alunoDao.existsByMatricula(aluno.getMatricula()))
+                .map(al -> objectMapperUtil.map(this.alunoDao.save(al), AlunoResponseDto.class))
+                .orElseThrow(
+                        () -> new BusinessException(!alunoExists
+                                ? BusinessExceptionMessage.NOT_FOUND.getMensagem()
+                                : BusinessExceptionMessage.ATTRIBUTE_VALUE_ALREADY_EXISTS.getMensagemValorJaExiste("matrícula")
+                        )
+                );
+
     }
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Deleta um aluno da base de dados.
-     *
-     * @param aluno - O Aluno que será deletado da base de dados.
+     * @param id O id do aluno que será deletado da base de dados.
      *
      */
     @Override
-    public void deletarAluno(Aluno aluno) {
-        if(aluno == null) {
-            throw new BusinessException(ALUNO_NULL);
-        } 
-        if(alunoDao.existsById(aluno.getId()) == false) {
-            throw new BusinessException(ALUNO_NAO_EXISTE);
-        }
-        alunoDao.delete(aluno);
+    public AlunoResponseDto deletarAlunoPorId(UUID id) {
+
+        return this.alunoDao.findById(id)
+                .map(al -> {
+                    this.alunoDao.delete(al);
+                    return objectMapperUtil.map(al, AlunoResponseDto.class);
+                })
+                .orElseThrow(() -> new BusinessException(BusinessExceptionMessage.NOT_FOUND.getMensagem()));
+
     }
 
-    /**
-     *
-     * Lista todos os alunos presentes na base de dados.
-     *
-     * @return uma lista com todos os alunos da base de dados, ou nulo caso não existe nenhum.
-     */
-    @Override
-    public List<Aluno> listarAlunos() {
-        return this.alunoDao.findAll();
-    }
 
     /**
      * AVISO: Temporariamente desabilitada enquanto não é entendida a sua utilidade.
--- src/main/java/br/com/ifba/entity/aluno/service/IAlunoService.java
+++ src/main/java/br/com/ifba/entity/aluno/service/IAlunoService.java
@@ -1,21 +1,37 @@
 package br.com.ifba.entity.aluno.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import br.com.ifba.entity.aluno.dto.AlunoResponseDto;
 import br.com.ifba.entity.aluno.model.Aluno;
 
-public interface IServiceAluno {
+/**
+ * @author Unknown
+ * Editado por Giovane Neves
+ * @since Desde V1.0.1
+ */
+public interface IAlunoService {
 
     // =========================================================== //
     // =============== [ ASSINATURAS DE MÉTODOS ] ================ //
     // =========================================================== //
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * Lista todos os alunos presentes na base de dados.
+     * @return uma lista com todos os alunos da base de dados, ou nulo caso não existe nenhum.
+     */
+    List<AlunoResponseDto> listarAlunos();
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Salva um aluno na base de ddados.
-     *
-     * @param aluno - O Aluno que será salvo na base de dados.
+     * @param aluno O Aluno que será salvo na base de dados.
      * @return um objeto DTO com os dados do aluno salvo na base de dados,
      * caso a operação ocorra com sucesso.
      *
@@ -23,37 +39,30 @@ public interface IServiceAluno {
     AlunoResponseDto salvarAluno(Aluno aluno);
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Atualiza os dados de uma aluno existente na base de dados.
-     *
-     * @param aluno - O Aluno que terá os seus dados atualizados.
+     * @param aluno O Aluno que terá os seus dados atualizados.
      * @return os dados atualizados do aluno.
-     *
      */
-    Aluno atualizarAluno(Aluno aluno);
+    AlunoResponseDto atualizarAluno(Aluno aluno);
 
     /**
-     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
      * Deleta um aluno da base de dados.
-     *
-     * @param aluno - O Aluno que será deletado da base de dados.
-     *
+     * @param id O ID do aluno que será deletado da base de dados.
      */
-    void deletarAluno(Aluno aluno);
-
-    /**
-     *
-     * Lista todos os alunos presentes na base de dados.
-     *
-     * @return uma lista com todos os alunos da base de dados, ou nulo caso não existe nenhum.
-     */
-    List<Aluno> listarAlunos();
+    AlunoResponseDto deletarAlunoPorId(UUID id);
 
     /**
      *
      * AVISO: Temporariamente desabilitada enquanto não é entendida a sua utilidade.
      *
      */
+    @Deprecated
     List<Aluno> findByStatusAlunoId(Long id);
 
 }

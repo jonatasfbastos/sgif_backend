97a357d26965a54972cfdf7746ade6bb40f3166a
andesson reis <andessonreys@gmail.com>
2023-10-20 14:59:44 -0300

    feat: Criando endpoint para listar pessoas

--- src/main/java/br/com/ifba/controller/v1/PessoaController.java
+++ src/main/java/br/com/ifba/controller/v1/PessoaController.java
@@ -1,6 +1,9 @@
 package br.com.ifba.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +29,20 @@ public class PessoaController {
     // =========================================================== //
     // =============== [        ENDPOINTS       ] ================ //
     // =========================================================== //
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Obtém a lista de todos as pessoas.
+     * 
+     * @return Uma lista de pessoas ou uma resposta de erro em caso de falha.
+     * 
+     * @author Andesson Reis
+     */
+    @GetMapping(path = "/pessoas", consumes = "application/json")
+    public ResponseEntity<?> getPessoas() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pessoaService.getAllPessoas());
+    }
 }

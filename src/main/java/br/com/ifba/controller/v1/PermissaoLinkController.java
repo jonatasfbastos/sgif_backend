4abd6c54f5a3e5ff12f53ddbd5d82034870ab5dd
andesson reis <andessonreys@gmail.com>
2023-10-19 23:39:34 -0300

    feat: Criado endpoint para 'GetAll' links

--- src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
+++ src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
@@ -1,6 +1,9 @@
 package br.com.ifba.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.ifba.entity.permissaolink.service.IServicePermissaoLink;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
@@ -19,4 +22,22 @@ public class PermissaoLinkController {
     // =========================================================== //
     // =============== [        ENDPOINTS       ] ================ //
     // =========================================================== //
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Obtém a lista de todos os links de permissões.
+     * 
+     * @return Uma lista de links ou uma resposta de erro em caso de falha.
+     *         
+     * 
+     * @author Andesson Reis
+     */
+
+    @GetMapping(path = "/listarLinks", consumes = "application/json")
+    public ResponseEntity<?> listarLinks() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(permissaoLinkService.getAllLinks());
+    }
 }

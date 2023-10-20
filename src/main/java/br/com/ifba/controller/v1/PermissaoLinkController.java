488c0f5915cc110432a7566a6e0047d1985fcfd1
andesson reis <andessonreys@gmail.com>
2023-10-20 00:21:16 -0300

    feat: Criado endpoint para 'DELETE' links

--- src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
+++ src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,6 +105,20 @@ public class PermissaoLinkController {
             ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
             : ResponseEntity.status(HttpStatus.CREATED)
             .body(permissaoLinkService.saveLink(permissaoLink));
+    }
+
+     /**
+     * @author Andesson Reis
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Deleta uma permissão.
+     * @return uma entidade de resposta generica.
+     */
+    @DeleteMapping(path = "/permissoesLinks/permissoesLink/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletaPermissaoLinkId (@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(permissaoLinkService.deleteLink(id));
     }
 
 }

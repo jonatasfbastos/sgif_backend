f37866b44398be7abf574b2a91a3fda3955d31a0
andesson reis <andessonreys@gmail.com>
2023-10-20 00:11:36 -0300

    feat: Criado endpoint para 'UPDATE' links

--- src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
+++ src/main/java/br/com/ifba/controller/v1/PermissaoLinkController.java
@@ -8,9 +8,15 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.permissaolink.dto.PermissaoLinkRequestDto;
+import br.com.ifba.entity.permissaolink.model.PermissaoLink;
 import br.com.ifba.entity.permissaolink.service.IServicePermissaoLink;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -61,6 +67,22 @@ public class PermissaoLinkController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(permissaoLinkService.getAllByPermissaoId(id));
+    }
+
+    /**
+     * @author Andesson Reis
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Atualiza uma permissaoLink.
+     * @return uma entidade de resposta generica.
+     */
+    @PutMapping(path = "/permissoesLinks/permissoesLink", consumes = "application/json")
+    public ResponseEntity<?> atualizarPermissaoLinks (@Valid @RequestBody PermissaoLinkRequestDto permissaoLinkDto, BindingResult result){
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(permissaoLinkService.updateLink(objectMapperUtil.map(permissaoLinkDto, PermissaoLink.class)));
+
     }
 
 }

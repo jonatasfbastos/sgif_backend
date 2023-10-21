85660c2ec7fdcf78e86419876d44ac7a6c374e3e
andesson reis <andessonreys@gmail.com>
2023-10-21 19:27:48 -0300

    feat: Criado endpoint de 'GetAll'

--- src/main/java/br/com/ifba/controller/v1/FornecedorController.java
+++ src/main/java/br/com/ifba/controller/v1/FornecedorController.java
@@ -1,6 +1,9 @@
 package br.com.ifba.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,17 @@ public class FornecedorController {
 
     @Autowired
     ObjectMapperUtil objectMapperUtil;
+
+    /**
+     * Obtém a lista de todos os fornecedores.
+     * 
+     * @return Uma lista de fornecedores ou uma resposta de erro em caso de falha.
+     */
+    @GetMapping(path = "/fornecedores", consumes = "application/json")
+    public ResponseEntity<?> getFornecedores() {
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(fornecedorService.getAllFornecedores());
+    }
 
 }

33396c1df5d4975c957f4524281c22b94b0a3ecb
andesson reis <andessonreys@gmail.com>
2023-10-20 23:19:22 -0300

    refactor: adicionando metodo findById por UUID

--- src/main/java/br/com/ifba/entity/setor/dao/IDaoSetor.java
+++ src/main/java/br/com/ifba/entity/setor/dao/IDaoSetor.java
@@ -1,10 +1,13 @@
 package br.com.ifba.entity.setor.dao;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.ifba.entity.requisicao.model.Requisicao;
 import br.com.ifba.entity.setor.model.Setor;
 
 /**
@@ -15,5 +18,16 @@ import br.com.ifba.entity.setor.model.Setor;
 public interface IDaoSetor extends JpaRepository<Setor, Long> {
 
    List<Setor> findByNome(String name);
+
+   /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Encontra um setor pelo ID passado por parâmetro.
+     * @param id O ID do setor a ser enconotrado.
+     * @return um objeto opcional do tipo de setor,
+     * que será nulo caso o setor não exista na base de dados.
+     */
+    Optional<Setor> findById(UUID id);
 
 }

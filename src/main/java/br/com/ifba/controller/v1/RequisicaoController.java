4ad7061bbe5a58da6f8ae0e7294d8da0d36d46a0
andesson reis <andessonreys@gmail.com>
2023-10-20 21:41:46 -0300

    feat: Criado endpoint 'GetAll'

--- src/main/java/br/com/ifba/controller/v1/RequisicaoController.java
+++ src/main/java/br/com/ifba/controller/v1/RequisicaoController.java
@@ -1,11 +1,18 @@
 package br.com.ifba.controller.v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.entity.requisicao.service.IServiceRequisicao;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+
 /**
  * @author Andesson reis
- * Desde V1.0.1
+ * @since V1.0.1
  * 
  *Veja também: {@link br.com.ifba.entity.requisicao.model.Requisicao}
  */
@@ -13,5 +20,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/apif/v1")
 public class RequisicaoController {
-    
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+    @Autowired
+    IServiceRequisicao requisicaoService;
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [        ENDPOINTS       ] ================ //
+    // =========================================================== //
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Obtém a lista de todos as requisicao.
+     * 
+     * @return Uma lista de requisicao ou uma resposta de erro em caso de falha.
+     * 
+     * @author Andesson Reis
+     */
+    @GetMapping(path = "/requisicoes", consumes = "application/json")
+    public ResponseEntity<?> getRequisicoes() {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(requisicaoService.getAllRequisicao());
+    }
 }

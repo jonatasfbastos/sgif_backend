362dbab8dc4532d59de6fef24acc1234e90cbf7e
andesson reis <andessonreys@gmail.com>
2023-10-21 18:55:47 -0300

    refactor: Atualizando metodo de 'SAVE'

--- src/main/java/br/com/ifba/entity/fornecedor/service/ServiceFornecedor.java
+++ src/main/java/br/com/ifba/entity/fornecedor/service/ServiceFornecedor.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.fornecedor.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,41 +6,54 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.com.ifba.entity.fornecedor.dao.IDaoFornecedor;
+import br.com.ifba.entity.fornecedor.dto.FornecedorResponseDto;
 import br.com.ifba.entity.fornecedor.model.Fornecedor;
+import br.com.ifba.entity.perfilusuario.dao.IDaoPerfilUsuario;
+import br.com.ifba.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 /**
- *
  * @author vitor
+ * @Editado por Andesson Reis
+ * @since V1.0.1
+ * 
+ * Service que fornece operações relacionadas a perfis de usuário
+ * 
+ * Esta classe contém métodos para criar, atualizar, excluir e recuperar
+ * informações de Fornecedor.
+ *
  */
 @Service
 public class ServiceFornecedor implements IServiceFornecedor {
 
-    // Constantes para condições de erro
+     // =========================================================== //
+    // =============== [ ATRIBUTOS ] ================ //
+    // =========================================================== //
 
-    // Fornecedor Null
-    public final static String FORNECEDOR_NULL = "Fornecedor null";
-
-    // Fornecedor já existe
-    public final static String FORNECEDOR_EXISTE = "Fornecedor já existe";
-
-    // Fornecedor não existente
-    public final static String FORNECEDOR_NAO_EXISTE = "O Fornecedor já existe na base de dados";
-
-    // Fornecedor inválido
-    public final static String FORNECEDOR_INVALIDO = "Fornecedor inválido";
-
-    // Criando objeto de instância
     @Autowired
     private IDaoFornecedor daoFornecedor;
 
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [ MÉTODOS ] ================== //
+    // =========================================================== //
+
+   /**
+     * Salva um fornecedor (Response DTO) na base de dados.
+     *
+     * @param fornecedor O fornecedor (Response DTO) a ser salvo.
+     * @return O fornecedor (Response DTO) salvo.
+     * @author Andesson Reis
+     * @since V1.0.1
+     */
     @Override
-    public Fornecedor saveFornecedor(Fornecedor fornecedor) {
-        if (fornecedor == null) {
-            throw new BusinessException(FORNECEDOR_NULL);
-        } else {
-            return daoFornecedor.save(fornecedor);
-        }
+    public FornecedorResponseDto saveFornecedor(Fornecedor fornecedor) {
+        return objectMapperUtil.map(
+                daoFornecedor.save(fornecedor),
+                FornecedorResponseDto.class);
     }
 
     @Override

c3440102c516767992f9d5370cbed7f627732bbc
andesson reis <andessonreys@gmail.com>
2023-10-20 23:11:09 -0300

    refactor: Atualizando metodo de 'SAVE'

--- src/main/java/br/com/ifba/entity/setor/service/ServiceSetor.java
+++ src/main/java/br/com/ifba/entity/setor/service/ServiceSetor.java
@@ -1,50 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.setor.service;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import br.com.ifba.entity.setor.dao.IDaoSetor;
+import br.com.ifba.entity.setor.dto.SetorResponseDto;
 import br.com.ifba.entity.setor.model.Setor;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- *
  * @author Everton.
+ * @Editado por Andesson Reis
+ * @since V1.0.1
+ * 
+ * Service que fornece operações relacionadas a Setor
+ * 
+ * Esta classe contém métodos para criar, atualizar, excluir e recuperar
+ * informações de setor.
  */
 @Service
 public class ServiceSetor implements IServiceSetor {
 
-    // CONSTANTES
+    // =========================================================== //
+    // ======================= [ ATRIBUTOS ] ===================== //
+    // =========================================================== //
 
-    // mensagem de erro se o setor for null;
-    public final static String SETOR_NULL = "Setor null";
-
-    // mensagem de erro se o setor já existir;
-    public final static String SETOR_EXISTE = "O Setor já existe,";
-
-    // mensagem de erro se o setor não existir no banco;
-    public final static String SETOR_NAO_EXISTE = "O Setor não existe na base de dados";
-
-    // mensagem de erro se o setor for inválido;
-    public final static String SETOR_INVALIDO = "Setor inválido";
-
-    // Objeto
     @Autowired
     private IDaoSetor daoSetor;
 
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // ========================= [ MÉTODOS ] ===================== //
+    // =========================================================== //
+
+    /**
+     * Salva um setor na base de dados e retorna um objeto DTO com os dados
+     * do setor salvo.
+     *
+     * @param setor - O setor que será salvo na base de dados.
+     * @return um objeto DTO com os dados do setor salvo.
+     */
     @Override
-    public Setor saveSetor(Setor setor) {
-        if (setor == null) {
-            throw new BusinessException(SETOR_NULL);
-        } else {
-            return daoSetor.save(setor);
-        }
+    public SetorResponseDto saveSetor(@Valid Setor setor) {
+       
+        return objectMapperUtil.map(
+                daoSetor.save(setor),
+                SetorResponseDto.class);
     }
 
     @Override

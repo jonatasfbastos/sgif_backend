147ea521c8ea90c11eaa796b8c0f3553b40b1b94
andesson reis <andessonreys@gmail.com>
2023-10-20 22:00:55 -0300

    feat: Criado endpoint 'UPDATE'

--- src/main/java/br/com/ifba/controller/v1/RequisicaoController.java
+++ src/main/java/br/com/ifba/controller/v1/RequisicaoController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.requisicao.dto.RequisicaoRequestDto;
+import br.com.ifba.entity.requisicao.model.Requisicao;
 import br.com.ifba.entity.requisicao.service.IServiceRequisicao;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -37,9 +45,9 @@ public class RequisicaoController {
     /**
      * @apiNote Endpoint criado desde a versão 1.0.1
      *
-     * Obtém a lista de todos as requisicao.
+     * Obtém a lista de todos as requisição.
      * 
-     * @return Uma lista de requisicao ou uma resposta de erro em caso de falha.
+     * @return Uma lista de requisições ou uma resposta de erro em caso de falha.
      * 
      * @author Andesson Reis
      */
@@ -49,4 +57,23 @@ public class RequisicaoController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(requisicaoService.getAllRequisicao());
     }
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     *
+     * Atualiza uma requisição.
+     * 
+     * @return uma entidade de resposta generica.
+     * 
+     * @author Andesson Reis
+     */
+    @PutMapping(path = "/requisicoes/requisicao", consumes = "application/json")
+    public ResponseEntity<?> atualizarRequisicao(@Valid @RequestBody RequisicaoRequestDto reqDto, BindingResult result){
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(requisicaoService.updateRequisicao(objectMapperUtil.map(reqDto, Requisicao.class)));
+
+    }
+
 }

d1f97dd76b05104d6bac57f7c1c35bc41a577466
NekoYasha <autornekoyasha@gmail.com>
2023-10-21 10:38:31 -0300

    ✏ refactor: Reescrevendo métodos do service

---
+++ src/main/java/br/com/ifba/entity/turma/dto/TurmaResponseDto.java
@@ -0,0 +1,2 @@
+package br.com.ifba.entity.turma.dto;public class TurmaResponseDto {
+}
---
+++ src/main/java/br/com/ifba/entity/turma/service/ITurmaService.java
@@ -0,0 +1,56 @@
+package br.com.ifba.entity.turma.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import br.com.ifba.entity.turma.dto.TurmaResponseDto;
+import br.com.ifba.entity.turma.model.Turma;
+
+/**
+ * @author Giovane Neves
+ * @since Desde V1.0.1
+ */
+public interface ITurmaService {
+
+    // =========================================================== //
+    // =============== [ ASSINATURAS DE MÉTODOS ] ================ //
+    // =========================================================== //
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * @return uma lista com todas as turmas da base de dados, ou nulo, caso não exista nenhuma.
+     */
+    List<TurmaResponseDto> listarTurmas();
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * @param turma A turma que será salva na base de dados.
+     * @return um objeto DTO com os dados da turma salva na base de dados,
+     * caso a operação ocorra com sucesso.
+     */
+    TurmaResponseDto salvarTurma(Turma turma);
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * @param turma A turma que terá os seus dados atualizados.
+     * @return um DTO com os dados atualizados da turma.
+     */
+    TurmaResponseDto atualizarTurma(Turma turma);
+
+    /**
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * <p>
+     * @param id O ID da turma que será deletado da base de dados.
+     * @return um DTO com os dados da turma deletada da base de dados.
+     */
+    TurmaResponseDto deletarTurmaPorId(UUID id);
+
+
+}

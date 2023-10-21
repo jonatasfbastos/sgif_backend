a4d56e16acd55b16888af9880830df1a02f2bc5c
andesson reis <andessonreys@gmail.com>
2023-10-20 21:33:44 -0300

    feat: Criado controller para requisição

---
+++ src/main/java/br/com/ifba/controller/v1/RequisicaoController.java
@@ -0,0 +1,17 @@
+package br.com.ifba.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Andesson reis
+ * Desde V1.0.1
+ * 
+ *Veja também: {@link br.com.ifba.entity.requisicao.model.Requisicao}
+ */
+
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class RequisicaoController {
+    
+}

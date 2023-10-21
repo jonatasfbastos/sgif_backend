d6d80f2cac85349bf06f4abd477461f4d2acd3c5
andesson reis <andessonreys@gmail.com>
2023-10-21 19:36:12 -0300

    feat: Criado endpoint de 'UPDATE'

--- src/main/java/br/com/ifba/controller/v1/FornecedorController.java
+++ src/main/java/br/com/ifba/controller/v1/FornecedorController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.fornecedor.dto.FornecedorRequestDto;
+import br.com.ifba.entity.fornecedor.model.Fornecedor;
 import br.com.ifba.entity.fornecedor.service.IServiceFornecedor;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -37,9 +45,23 @@ public class FornecedorController {
      */
     @GetMapping(path = "/fornecedores", consumes = "application/json")
     public ResponseEntity<?> getFornecedores() {
-        
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(fornecedorService.getAllFornecedores());
     }
 
+    /**
+     * Atualiza um fornecedor.
+     * 
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(path = "/fornecedores/fornecedor", consumes = "application/json")
+    public ResponseEntity<?> atualizarFornecedor(@Valid @RequestBody FornecedorRequestDto fornecedorDto, BindingResult result) {
+
+        
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(fornecedorService.updateFornecedor(objectMapperUtil.map(fornecedorDto, Fornecedor.class)));
+
+    }
 }

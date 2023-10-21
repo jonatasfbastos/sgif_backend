02e21749bd4b293437d4e0201f37c62aeee96f4c
andesson reis <andessonreys@gmail.com>
2023-10-21 19:00:37 -0300

    clean: removendo anotações desnecessarias

--- src/main/java/br/com/ifba/entity/fornecedor/dao/IDaoFornecedor.java
+++ src/main/java/br/com/ifba/entity/fornecedor/dao/IDaoFornecedor.java
@@ -1,18 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package br.com.ifba.entity.fornecedor.dao;
 
 import java.util.Optional;
 import java.util.UUID;
-
-//import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ifba.entity.fornecedor.model.Fornecedor;
-import br.com.ifba.entity.item.model.Item;
 
 /**
  *

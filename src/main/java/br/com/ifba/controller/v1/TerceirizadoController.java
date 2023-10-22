88f41d55077219d74ec87853b2f06c05ad8f6f79
andesson reis <andessonreys@gmail.com>
2023-10-22 15:05:09 -0300

    feat: Criado endpoint para 'SAVE'

--- src/main/java/br/com/ifba/controller/v1/TerceirizadoController.java
+++ src/main/java/br/com/ifba/controller/v1/TerceirizadoController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.terceirizado.dto.TerceirizadoRequestDto;
+import br.com.ifba.entity.terceirizado.model.Terceirizado;
 import br.com.ifba.entity.terceirizado.service.IServiceTerceirizado;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -47,5 +55,19 @@ public class TerceirizadoController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(terceirizadoService.getAllTerceirizados());
+    }
+
+    
+    /**
+     * Salva um terceirizado.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PostMapping(path = "/terceirizados/terceirizado", consumes = "application/json")
+    public ResponseEntity<?> salvarTerceirizado(@Valid @RequestBody TerceirizadoRequestDto terceirizadoDto, BindingResult result) {
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(terceirizadoService.saveTerceirizado(objectMapperUtil.map(terceirizadoDto, Terceirizado.class)));
     }
 }

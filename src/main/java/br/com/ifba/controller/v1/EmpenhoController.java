0feeab2b099ac9c3efc7a1436adc257816f2e242
andesson reis <andessonreys@gmail.com>
2023-10-21 22:41:32 -0300

    feat: Criado endpoit para 'UPDATE'

--- src/main/java/br/com/ifba/controller/v1/EmpenhoController.java
+++ src/main/java/br/com/ifba/controller/v1/EmpenhoController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.empenho.dto.EmpenhoRequestDto;
+import br.com.ifba.entity.empenho.model.Empenho;
 import br.com.ifba.entity.empenho.service.IServiceEmpenho;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -41,4 +49,19 @@ public class EmpenhoController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(empenhoService.getAllEmpenho());
     }
+
+     /**
+     * Atualiza um empenho.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(path = "/empenhos/empenho", consumes = "application/json")
+    public ResponseEntity<?> atualizarEmpenho(@Valid @RequestBody EmpenhoRequestDto empenhoDto, BindingResult result) {
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(empenhoService.updateEmpenho(objectMapperUtil.map(empenhoDto, Empenho.class)));
+    }
+
+   
 }

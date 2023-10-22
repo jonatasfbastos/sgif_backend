7fe1630c2bcc1ad4422b983bc313269bd226fbea
andesson reis <andessonreys@gmail.com>
2023-10-22 15:31:38 -0300

    refactor: Atualizando entidade

--- src/main/java/br/com/ifba/entity/relatoriomensal/model/RelatorioMensal.java
+++ src/main/java/br/com/ifba/entity/relatoriomensal/model/RelatorioMensal.java
@@ -10,19 +10,53 @@ import br.com.ifba.entity.item.model.Item;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * Esta classe representa um relatório mensal.
+ *
+ * @author unknown
+ * @Editado por Andesson Reis
+ * @since V1.0.1
+ */
 @Entity
-@Table(name = "rna")
+@Table(name = "relatorio_mensal")
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class RelatorioMensal extends PersistenceEntity {
-    String nome;
-    int qtdInicial;
-    int qtdEntrou;
-    int qtdSaiu;
-    float valorTotal;
-    float valorTotalSairam;
-    String data;
-    
+    /**
+     * O nome do relatório mensal. Não pode ser nulo.
+     */
+    private String nome;
+
+    /**
+     * A quantidade inicial.
+     */
+    private int qtdInicial;
+
+    /**
+     * A quantidade que entrou.
+     */
+    private int qtdEntrou;
+
+    /**
+     * A quantidade que saiu.
+     */
+    private int qtdSaiu;
+
+    /**
+     * O valor total.
+     */
+    private float valorTotal;
+
+    /**
+     * O valor total das saídas.
+     */
+    private float valorTotalSairam;
+
+    /**
+     * A data do relatório mensal.
+     */
+    private String data;
+
     @OneToOne
     @JoinColumn(name = "item_id")
     private Item item;

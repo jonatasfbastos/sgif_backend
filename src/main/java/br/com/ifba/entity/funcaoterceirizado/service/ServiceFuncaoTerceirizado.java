ac6c4792edbf5fefb95d5228c2e149b437126ba2
andesson reis <andessonreys@gmail.com>
2023-10-22 12:05:03 -0300

    refactor: Atualizando metodo de 'SAVE'

--- src/main/java/br/com/ifba/entity/funcaoterceirizado/service/ServiceFuncaoTerceirizado.java
+++ src/main/java/br/com/ifba/entity/funcaoterceirizado/service/ServiceFuncaoTerceirizado.java
@@ -1,44 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.funcaoterceirizado.service;
 
 import br.com.ifba.entity.funcaoterceirizado.model.FuncaoTerceirizado;
+
 import br.com.ifba.entity.funcaoterceirizado.dao.IDaoFuncaoTerceirizado;
+import br.com.ifba.entity.funcaoterceirizado.dto.FuncaoTerceirizadoResponseDto;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
+ * Service que fornece operações relacionadas a FuncaoTerceirizado
  *
  * @author ph715
+ * @since V1.0.1
+ * @Editado por Andesson Reis
  */
 @Service
-public class ServiceFuncaoTerceirizado implements IServiceFuncaoTerceirizado{
-    private final static String FUNCAO_NULL = "A função do técnico está vazia, preencha e tente novamente.";
-    private final static String FUNCAO_NAO_EXISTE = "A função informada não existe.";
-    private final static String FUNCAO_DELETADA = "Função deletada com sucesso.";
-    private final static String CAMPO_VAZIO = "Há campo vazio, preencha e tente novamente.";
-    
+public class ServiceFuncaoTerceirizado implements IServiceFuncaoTerceirizado {
+    // =========================================================== //
+    // ======================== [ ATRIBUTOS ] ==================== //
+    // =========================================================== //
+
     @Autowired
     private IDaoFuncaoTerceirizado daoFuncaoTerceirizado;
 
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // ======================== [ MÉTODOS ] ====================== //
+    // =========================================================== //
+
+    /**
+     * Salva uma Função de Terceirizado na base de dados e retorna um objeto DTO com
+     * os dados resumidos da Função salva.
+     *
+     * @param funcaoTerceirizado - A Função de Terceirizado que será salva na base
+     *                           de dados.
+     * @return um objeto DTO com os dados resumidos da Função salva.
+     * @author Andesson Reis
+     * @since V1.0.1
+     */
     @Override
-    public FuncaoTerceirizado saveFuncaoTerceirizado(FuncaoTerceirizado funcaoTerceirizado) {
-        if(funcaoTerceirizado == null){
-            throw new BusinessException(FUNCAO_NULL);
-        }
-        if(funcaoTerceirizado.getNome().isEmpty() || funcaoTerceirizado.getDescricao().isBlank()){
-            throw new BusinessException(CAMPO_VAZIO);
-        }
-        return daoFuncaoTerceirizado.save(funcaoTerceirizado);
+    public FuncaoTerceirizadoResponseDto saveFuncaoTerceirizado(FuncaoTerceirizado funcaoTerceirizado) {
+
+        return Optional.of(funcaoTerceirizado)
+                .filter(fun -> !this.daoFuncaoTerceirizado.existsByNome(fun.getNome()))
+                .map(fun -> objectMapperUtil.map(this.daoFuncaoTerceirizado.save(fun),
+                        FuncaoTerceirizadoResponseDto.class))
+                .orElseThrow(() -> new BusinessException(
+                        BusinessExceptionMessage.ATTRIBUTE_VALUE_ALREADY_EXISTS.getMensagemValorJaExiste("nome")));
     }
 
     @Override
     public String deleteFuncaoTerceirizado(Long id) {
-        if(daoFuncaoTerceirizado.existsById(id) == false){
+        if (daoFuncaoTerceirizado.existsById(id) == false) {
             throw new BusinessException(FUNCAO_NAO_EXISTE);
         }
         daoFuncaoTerceirizado.delete(daoFuncaoTerceirizado.getReferenceById(id));
@@ -49,5 +71,5 @@ public class ServiceFuncaoTerceirizado implements IServiceFuncaoTerceirizado{
     public List<FuncaoTerceirizado> getAllFuncoesTerceirizado() {
         return daoFuncaoTerceirizado.findAll();
     }
-    
+
 }

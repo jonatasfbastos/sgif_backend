84936f399da6b46d42c1cec0231888326ce4749f
andesson reis <andessonreys@gmail.com>
2023-10-22 20:21:51 -0300

    feat: Criado endpoint para 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/ServidorController.java
+++ src/main/java/br/com/ifba/controller/v1/ServidorController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -68,6 +73,18 @@ public class ServidorController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(servidorService.saveServidor(objectMapperUtil.map(servidorDto, Servidor.class)));
+    }
+
+    /**
+     * Deleta um servidor com base no seu ID.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/servidores/servidor/{id}", consumes = "application.json")
+    public ResponseEntity<?> deletarServidorPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(servidorService.deleteServidor(id));
     }
 
 }

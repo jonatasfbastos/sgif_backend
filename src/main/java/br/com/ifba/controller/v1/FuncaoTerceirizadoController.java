dc78a81fe805bf57cd3c7200f4942daa50d64ebc
andesson reis <andessonreys@gmail.com>
2023-10-22 13:12:46 -0300

    feat: Criado endpoint para SAVE

--- src/main/java/br/com/ifba/controller/v1/FuncaoTerceirizadoController.java
+++ src/main/java/br/com/ifba/controller/v1/FuncaoTerceirizadoController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.funcaoterceirizado.dto.FuncaoTerceirizadoRequestDto;
+import br.com.ifba.entity.funcaoterceirizado.model.FuncaoTerceirizado;
 import br.com.ifba.entity.funcaoterceirizado.service.IServiceFuncaoTerceirizado;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -48,6 +56,19 @@ public class FuncaoTerceirizadoController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(funcaoTerceirizadoService.getAllFuncoesTerceirizado());
+    }
+
+    /**
+     * Salva uma função de terceirizado.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PostMapping(path = "/funcoes-terceirizados/funcao-terceirizado", consumes = "application/json")
+    public ResponseEntity<?> salvarFuncaoTerceirizado(@Valid @RequestBody FuncaoTerceirizadoRequestDto funcaoTerceirizadoDto, BindingResult result) {
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(funcaoTerceirizadoService.saveFuncaoTerceirizado(objectMapperUtil.map(funcaoTerceirizadoDto, FuncaoTerceirizado.class)));
     }
 
 }

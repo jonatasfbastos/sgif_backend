f729e7c32893e8c7b1cc2c4b37d9852b909a6b27
andesson reis <andessonreys@gmail.com>
2023-10-22 10:34:10 -0300

    feat: Criado endpoint para 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/TipoDeItemController.java
+++ src/main/java/br/com/ifba/controller/v1/TipoDeItemController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -82,6 +87,18 @@ public class TipoDeItemController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(tipoDeItemService.saveTipoDeItem(objectMapperUtil.map(tipoDeItemDto, TipoDeItem.class)));
+    }
+
+    /**
+     * Deleta um tipo de item com base no seu ID.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/tiposdeitem/tipodeitem/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarTipoDeItemPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(tipoDeItemService.deleteTipoDeItem(id));
     }
 
 }

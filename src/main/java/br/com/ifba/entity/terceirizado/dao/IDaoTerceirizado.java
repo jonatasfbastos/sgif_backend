e50508ddb86c6272f9de38023939ecb4bb738d86
andesson reis <andessonreys@gmail.com>
2023-10-22 14:14:05 -0300

    refactor: Atualizando de 'LONG' para 'UUID'

--- src/main/java/br/com/ifba/entity/terceirizado/dao/IDaoTerceirizado.java
+++ src/main/java/br/com/ifba/entity/terceirizado/dao/IDaoTerceirizado.java
@@ -1,6 +1,8 @@
 package br.com.ifba.entity.terceirizado.dao;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import br.com.ifba.entity.terceirizado.model.Terceirizado;
 
@@ -11,5 +13,29 @@ import org.springframework.stereotype.Repository;
 public interface IDaoTerceirizado extends JpaRepository<Terceirizado, Long> {
     List<Terceirizado> findByNome(String nome);
     boolean existsByNome(String nome);
-    List<Terceirizado> findFuncaoTerceirizadoById(Long id);
+    List<Terceirizado> findFuncaoTerceirizadoById(UUID id);
+
+        /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Verifica se existe uma Terceirizado com o ID passado por parâmetro
+     * na base de dados.
+     * @param id O id do Terceirizado a ser consultado.
+     * @return 'true' caso haja um Terceirizado com o Id
+     * passado por parâmetro na base de dados,
+     * 'false' caso contrário.
+     */
+    boolean existsById(UUID id);
+
+    /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Encontra uma Terceirizado pelo ID passado por parâmetro.
+     * @param id O ID da Terceirizado a ser enconotrado.
+     * @return um objeto opcional do tipo Terceirizado,
+     * que será nulo caso o Terceirizado não exista na base de dados.
+     */
+    Optional<Terceirizado> findById(UUID id);
 }

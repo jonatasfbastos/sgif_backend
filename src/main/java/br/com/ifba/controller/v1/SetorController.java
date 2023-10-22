9abe1586c95360163c8765e355205913672581b8
andesson reis <andessonreys@gmail.com>
2023-10-22 10:42:17 -0300

    feat: Criado endpoint para 'UPDATE'

--- src/main/java/br/com/ifba/controller/v1/SetorController.java
+++ src/main/java/br/com/ifba/controller/v1/SetorController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.setor.dto.SetorRequestDto;
+import br.com.ifba.entity.setor.model.Setor;
 import br.com.ifba.entity.setor.service.IServiceSetor;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -49,5 +57,18 @@ public class SetorController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(setorService.getAllSetor());
+    }
+
+    /**
+     * Atualiza um setor.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(path = "/setores/setor", consumes = "application/json")
+    public ResponseEntity<?> atualizarSetor(@Valid @RequestBody SetorRequestDto setorDto, BindingResult result) {
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(setorService.updateSetor(objectMapperUtil.map(setorDto, Setor.class)));
     }
 }

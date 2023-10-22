71ce9972f3da757749a3a1aa5e690aebbedd5f51
andesson reis <andessonreys@gmail.com>
2023-10-22 15:41:11 -0300

    feat: Criado request Dto para relatorio mensal

---
+++ src/main/java/br/com/ifba/entity/relatoriomensal/dto/RelatorioMensalRequestDto.java
@@ -0,0 +1,53 @@
+package br.com.ifba.entity.relatoriomensal.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.com.ifba.entity.item.dto.ItemRequestDto;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Andesson Reis
+ * @since V1.0.1
+ * DTO para receber dados de requisições direcionadas à entidade 'RelatorioMensal'.
+ *
+ * @param nome - O nome do relatório mensal.
+ * @param qtdInicial - A quantidade inicial.
+ * @param qtdEntrou - A quantidade que entrou.
+ * @param qtdSaiu - A quantidade que saiu.
+ * @param valorTotal - O valor total.
+ * @param valorTotalSairam - O valor total das saídas.
+ * @param data - A data do relatório mensal.
+ */
+public record RelatorioMensalRequestDto(
+        @JsonProperty(value = "nome")
+        @NotNull(message = "O nome é obrigatório.")
+        @NotBlank(message = "O nome não pode ser vazio.")
+        @Size(min = 1, max = 255, message = "O nome deve ter entre 1 e 255 caracteres.")
+        String nome,
+
+        @JsonProperty(value = "qtdInicial")
+        int qtdInicial,
+
+        @JsonProperty(value = "qtdEntrou")
+        int qtdEntrou,
+
+        @JsonProperty(value = "qtdSaiu")
+        int qtdSaiu,
+
+        @JsonProperty(value = "valorTotal")
+        float valorTotal,
+
+        @JsonProperty(value = "valorTotalSairam")
+        float valorTotalSairam,
+
+        @JsonProperty(value = "data")
+        @NotNull(message = "A data é obrigatória.")
+        @NotBlank(message = "A data não pode ser vazia.")
+        String data,
+
+        @JsonProperty(value = "item_id")
+        ItemRequestDto item
+) {
+}

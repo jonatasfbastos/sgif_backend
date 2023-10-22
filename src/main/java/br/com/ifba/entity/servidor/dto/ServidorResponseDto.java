5ec357a65e2854dadbb19e1669d43732cca43395
andesson reis <andessonreys@gmail.com>
2023-10-22 16:41:52 -0300

    feat: Criado responseDto

---
+++ src/main/java/br/com/ifba/entity/servidor/dto/ServidorResponseDto.java
@@ -0,0 +1,22 @@
+package br.com.ifba.entity.servidor.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * @author Andesson Reis
+ * @since V1.0.1
+ * 
+ * DTO para enviar dados em resposta a requisições relacionadas à entidade 'Servidor'.
+ *
+ * @param siape - O número do SIAPE do servidor.
+ * @param funcaoServidor - Informações da função do servidor.
+ */
+public class ServidorResponseDto {
+
+    @JsonProperty(value = "siape")
+    private String siape;
+
+    @JsonProperty(value = "funcaoServidor")
+    private FuncaoServidorResponseDto funcaoServidor;
+}

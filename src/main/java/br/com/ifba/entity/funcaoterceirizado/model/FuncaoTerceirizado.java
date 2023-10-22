0093a065b59206592d7ca309454631ca5d2cd177
andesson reis <andessonreys@gmail.com>
2023-10-22 11:09:20 -0300

    refactor: Atualizando entidade

--- src/main/java/br/com/ifba/entity/funcaoterceirizado/model/FuncaoTerceirizado.java
+++ src/main/java/br/com/ifba/entity/funcaoterceirizado/model/FuncaoTerceirizado.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.funcaoterceirizado.model;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
@@ -11,6 +7,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -21,16 +18,30 @@ import lombok.EqualsAndHashCode;
 /**
  *
  * @author ph715
+ * @since V1.0.1
+ * @Editado por Andesson Reis
  */
 @Entity
 @Data
 @Table(name = "funcao_terceirizado")
 @EqualsAndHashCode(callSuper = false)
 public class FuncaoTerceirizado extends PersistenceEntity implements Serializable {
+    
+    /**
+     * O nome da função do terceirizado.
+     */
+    @Column(nullable = false)
     private String nome;
+
+    /**
+     * A descrição da função do terceirizado.
+     */
+    @Column
     private String descricao;
     
+    /**
+     * A lista de terceirizados associados a esta função.
+     */
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "funcaoTerceirizado")
     private List<Terceirizado> terceirizado;
-    
 }

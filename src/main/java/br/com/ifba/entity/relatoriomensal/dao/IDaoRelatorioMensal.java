c8a1892d3d5a3eda139766ca93b8b537ca6b5c7e
andesson reis <andessonreys@gmail.com>
2023-10-22 15:59:09 -0300

    refactor: Atualizando metodo de 'LONG' para 'UUID'

--- src/main/java/br/com/ifba/entity/relatoriomensal/dao/IDaoRelatorioMensal.java
+++ src/main/java/br/com/ifba/entity/relatoriomensal/dao/IDaoRelatorioMensal.java
@@ -1,10 +1,24 @@
 package br.com.ifba.entity.relatoriomensal.dao;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ifba.entity.relatoriomensal.model.RelatorioMensal;
 
 
+
 public interface IDaoRelatorioMensal extends JpaRepository<RelatorioMensal, Long>{
     
+    /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Encontra um RelatorioMensal pelo ID passado por parâmetro.
+     * @param id O ID do RelatorioMensal a ser enconotrado.
+     * @return um objeto opcional do tipo RelatorioMensal,
+     * que será nulo caso o RelatorioMensal não exista na base de dados.
+     */
+    Optional<RelatorioMensal> findById(UUID id);
 }

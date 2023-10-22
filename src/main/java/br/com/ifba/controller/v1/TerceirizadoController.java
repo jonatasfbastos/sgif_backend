8dc88f9bc64f272050269a8af3847a70a823d0bd
andesson reis <andessonreys@gmail.com>
2023-10-22 15:08:00 -0300

    feat: Criado endpoint para 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/TerceirizadoController.java
+++ src/main/java/br/com/ifba/controller/v1/TerceirizadoController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,5 +74,17 @@ public class TerceirizadoController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(terceirizadoService.saveTerceirizado(objectMapperUtil.map(terceirizadoDto, Terceirizado.class)));
+    }
+
+    /**
+     * Deleta um terceirizado com base no seu ID.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/terceirizados/terceirizado/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarTerceirizadoPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(terceirizadoService.deleteTerceirizado(id));
     }
 }

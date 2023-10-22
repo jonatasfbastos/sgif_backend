dbd4e77a3de6e52c097f41a9926a884d290a6312
NekoYasha <autornekoyasha@gmail.com>
2023-10-22 19:00:35 -0300

    ✏ refactor: Renomeando DAO

--- src/main/java/br/com/ifba/entity/etapacurso/dao/IEtapaCursoDao.java
+++ src/main/java/br/com/ifba/entity/etapacurso/dao/IEtapaCursoDao.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import br.com.ifba.entity.etapacurso.model.EtapaCurso;
 
 @Repository
-public interface IDaoEtapaCurso extends JpaRepository<EtapaCurso, Long>{
+public interface IEtapaCursoDao extends JpaRepository<EtapaCurso, Long>{
     List<EtapaCurso> findByNome(String nome);
     boolean existsByNome(String nome);
 

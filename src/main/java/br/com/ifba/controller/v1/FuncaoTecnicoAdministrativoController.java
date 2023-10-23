6e6b74cd6507ea1ace790e45620ca062a49bf604
andesson reis <andessonreys@gmail.com>
2023-10-23 09:32:59 -0300

    fix: Resolvido problema com os importes

--- src/main/java/br/com/ifba/controller/v1/FuncaoTecnicoAdministrativoController.java
+++ src/main/java/br/com/ifba/controller/v1/FuncaoTecnicoAdministrativoController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

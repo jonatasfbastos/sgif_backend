42afbd588d7c1800e1e9f9b2c13954bebbbd03a2
andesson reis <andessonreys@gmail.com>
2023-10-23 00:53:56 -0300

    refactor: Atualizando metodo de delete

--- src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/service/ServiceFuncaoTecnicoAdministrativo.java
+++ src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/service/ServiceFuncaoTecnicoAdministrativo.java
@@ -4,11 +4,13 @@ import br.com.ifba.entity.funcaotecnicoadministrativo.dao.IDaoFuncaoTecnicoAdmin
 import br.com.ifba.entity.funcaotecnicoadministrativo.dto.FuncaoTecnicoAdministrativoResponseDto;
 import br.com.ifba.entity.funcaotecnicoadministrativo.model.FuncaoTecnicoAdministrativo;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.exception.BusinessExceptionMessage;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Service que fornece operações relacionadas a Função de Técnico Administrativo.
@@ -39,18 +41,29 @@ public class ServiceFuncaoTecnicoAdministrativo implements IServiceFuncaoTecnico
      */
     @Override
     public FuncaoTecnicoAdministrativoResponseDto saveFuncaoTecnicoAdm(FuncaoTecnicoAdministrativo funcaoTecnicoAdm) {
+        
         return objectMapperUtil.map(
                 funcaoTecnicoAdministrativoDao.save(funcaoTecnicoAdm),
                 FuncaoTecnicoAdministrativoResponseDto.class);
     }
+
+    /**
+     * Deleta uma Função Técnico Administrativo com base no ID.
+     *
+     * @param id - O ID da Função Técnico Administrativo a ser deletada.
+     * @return um objeto DTO com os dados da Função Técnico Administrativo deletada.
+     * @author Andesson Reis
+     * @since V1.0.1
+     */
     @Override
-    public String deleteFuncaoTecnicoAdm(Long id) {
-        if (daoFuncaoTecnicoAdministrativo.existsById(id) == false) {
-            throw new BusinessException(FUNCAO_NAO_EXISTE);
-        }
-        daoFuncaoTecnicoAdministrativo
-                .delete(daoFuncaoTecnicoAdministrativo.getReferenceById(id));
-        return FUNCAO_DELETADA;
+    public FuncaoTecnicoAdministrativoResponseDto deleteFuncaoTecnicoAdm(UUID id) {
+
+        return this.funcaoTecnicoAdministrativoDao.findById(id)
+                .map(funcaoTecnicoAdm -> {
+                    funcaoTecnicoAdministrativoDao.delete(funcaoTecnicoAdm);
+                    return objectMapperUtil.map(funcaoTecnicoAdm, FuncaoTecnicoAdministrativoResponseDto.class);
+                })
+                .orElseThrow(() -> new BusinessException(BusinessExceptionMessage.NOT_FOUND.getMensagem()));
     }
 
     @Override

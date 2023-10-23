4f0e94f1e041bb933f6c1363729c66139e20a309
andesson reis <andessonreys@gmail.com>
2023-10-23 12:56:02 -0300

    feat: Criado endpoit de getAll

--- src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
+++ src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
@@ -1,6 +1,9 @@
 package br.com.ifba.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,17 @@ public class ModalidadeController {
 
     @Autowired
     ObjectMapperUtil objectMapperUtil;
+
+    /**
+     * Obtém a lista de todas as modalidades.
+     *
+     * @return Uma lista de modalidades ou uma resposta de erro em caso de falha.
+     */
+    @GetMapping(consumes = "application/json")
+    public ResponseEntity<?> getModalidades() {
+        
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(modalidadeService.getAllModalidade());
+    }
 
 }

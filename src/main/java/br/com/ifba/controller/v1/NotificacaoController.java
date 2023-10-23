54c7c1006499f520695408aea792cb51a38d8142
andesson reis <andessonreys@gmail.com>
2023-10-23 09:45:27 -0300

    feat: Criado controller para notificação

---
+++ src/main/java/br/com/ifba/controller/v1/NotificacaoController.java
@@ -0,0 +1,36 @@
+package br.com.ifba.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifba.entity.curso.service.ICursoService;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
+
+/**
+ * @apiNote Endpoint criado desde a versão 1.0.1
+ *
+ * Controller para gerenciar notificações.
+ *
+ * @author Andesson Rei
+ * @since V1.0.1
+ *
+ * Veja também: {@link br.com.ifba.entity.notificacao.model.Notificacao}
+ */
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class NotificacaoController {
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+    @Autowired
+    ICursoService cursoService;
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [        ENDPOINTS       ] ================ //
+    // =========================================================== //
+}

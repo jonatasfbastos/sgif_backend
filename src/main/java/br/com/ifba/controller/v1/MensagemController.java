d481dc798f764a9c99e50d8702d0896a21c0f1ad
andesson reis <andessonreys@gmail.com>
2023-10-23 10:51:21 -0300

    feat: Criado endpoint para getall

--- src/main/java/br/com/ifba/controller/v1/MensagemController.java
+++ src/main/java/br/com/ifba/controller/v1/MensagemController.java
@@ -1,6 +1,11 @@
 package br.com.ifba.controller.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifba.entity.mensagem.service.IServiceMensagem;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
@@ -8,7 +13,8 @@ import br.com.ifba.infrastructure.util.ObjectMapperUtil;
  * @author Andesson reis
  * @since V1.0.1
  */
-
+@RestController
+@RequestMapping(path = "/apif/v1")
 public class MensagemController {
     // =========================================================== //
     // =============== [        ATRIBUTOS       ] ================ //
@@ -20,8 +26,17 @@ public class MensagemController {
     @Autowired
     ObjectMapperUtil objectMapperUtil;
 
-
     // =========================================================== //
     // =============== [         ENDPOINTS      ] ================ //
     // =========================================================== //
+
+    /**
+     * Obtém a lista de todas as mensagens.
+     *
+     * @return Uma lista de mensagens ou uma resposta de erro em caso de falha.
+     */
+    @GetMapping(consumes = "application/json")
+    public ResponseEntity<?> getMensagens() {
+        return ResponseEntity.status(HttpStatus.OK).body(mensagemService.getAllMensagem());
+    }
 }

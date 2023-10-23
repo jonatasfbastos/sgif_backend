0dd536d9f45f0a4758984de5161f495696012780
andesson reis <andessonreys@gmail.com>
2023-10-23 00:07:24 -0300

    feat: Criado request dto

---
+++ src/main/java/br/com/ifba/entity/tecnicoadministrativo/dto/TecnicoAdministrativoRequestDto.java
@@ -0,0 +1,26 @@
+package br.com.ifba.entity.tecnicoadministrativo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import br.com.ifba.entity.servidor.dto.ServidorRequestDto;
+import br.com.ifba.entity.funcaoservidor.dto.FuncaoServidorRequestDto;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Andesson Reis
+ * 
+ * DTO para receber dados de requisições direcionadas à entidade 'TecnicoAdministrativo'.
+ *
+ * @param funcaoServidor - Informações da função de servidor do técnico administrativo.
+ * @param funcaoTecnicoAdministrativo - Informações da função de técnico administrativo do servidor.
+ */
+public class TecnicoAdministrativoRequestDto {
+
+    @JsonProperty(value = "funcaoServidor")
+    @NotNull(message = "A função de servidor é obrigatória para o técnico administrativo.")
+    private FuncaoServidorRequestDto funcaoServidor;
+
+    @JsonProperty(value = "funcaoTecnicoAdministrativo")
+    @NotNull(message = "A função de técnico administrativo é obrigatória para o servidor.")
+    private FuncaoTecnicoAdministrativoRequestDto funcaoTecnicoAdministrativo;
+}

8727d4036b0c22b1d9ac86446d25696ceff44da6
andesson reis <andessonreys@gmail.com>
2023-10-23 16:04:55 -0300

    feat: Criado controller para matriz curricular

---
+++ src/main/java/br/com/ifba/controller/v1/MatrizCurricularController.java
@@ -0,0 +1,18 @@
+package br.com.ifba.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @apiNote Endpoint criado desde a versão 1.0.1
+ *
+ * Controller para gerenciar Matriz curricular .
+ *
+ * @author Andesson Reis
+ * @since V1.0.1
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class MatrizCurricularController {
+    
+}

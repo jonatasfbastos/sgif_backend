7c7a691c009362ad7b448ba98788db34ff018883
andesson reis <andessonreys@gmail.com>
2023-10-23 10:09:35 -0300

    refactor: Atalizando interface

--- src/main/java/br/com/ifba/entity/mensagem/service/IServiceMensagem.java
+++ src/main/java/br/com/ifba/entity/mensagem/service/IServiceMensagem.java
@@ -1,28 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.com.ifba.entity.mensagem.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import br.com.ifba.entity.mensagem.dto.MensagemResponseDto;
 import br.com.ifba.entity.mensagem.model.Mensagem;
 
-import java.util.List;
-
 /**
+ * Interface de serviço para Mensagem.
  *
  * @author felipe
+ * @Editado por Andesson Reis
+ * @since V1.0.1
  */
 public interface IServiceMensagem {
-    // Salva Mensagem
-     public abstract Mensagem saveMensagem(Mensagem mensagem);
-     // Atualiza Mensagem
-     public abstract Mensagem updateMensagem(Mensagem mensagem);
-     // Deleta Mensagem
-     public abstract void deleteMensagem(Mensagem mensagem);
-      // Metodo que retorna todos as Mensagem salvos no base de dados
-     public abstract List<Mensagem> getAllMensagem();
-     // Metodo que retorna a Mensagem com o id informado
-     public Mensagem findById(Long id);
-     // Metodo que retorna a lista com as Mensagem com o nome informado
-     public List<Mensagem> findByNome(String nome);
+
+    /**
+     * Salva uma Mensagem na base de dados e retorna um objeto DTO com os dados resumidos da Mensagem salva.
+     *
+     * @param mensagem - A Mensagem que será salva na base de dados.
+     * @return um objeto DTO com os dados resumidos da Mensagem salva.
+     */
+    MensagemResponseDto saveMensagem(Mensagem mensagem);
+
+    /**
+     * Atualiza uma Mensagem na base de dados e retorna um objeto DTO com os dados resumidos da Mensagem atualizada.
+     *
+     * @param mensagem - A Mensagem que será atualizada na base de dados.
+     * @return um objeto DTO com os dados resumidos da Mensagem atualizada.
+     */
+    MensagemResponseDto updateMensagem(Mensagem mensagem);
+
+    /**
+     * Deleta uma Mensagem com base no ID.
+     *
+     * @param id - O ID da Mensagem a ser deletada.
+     * @return um objeto DTO com os dados da Mensagem deletada.
+     */
+    MensagemResponseDto deleteMensagem(UUID id);
+
+    /**
+     * Obtém uma lista de todas as Mensagens como objetos DTO.
+     *
+     * @return uma lista de objetos DTO representando as Mensagens.
+     */
+    List<MensagemResponseDto> getAllMensagem();
 }

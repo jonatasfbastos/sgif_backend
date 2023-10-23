45789f5fa72789afbea4aff54b27cecdb35be09a
andesson reis <andessonreys@gmail.com>
2023-10-23 00:53:21 -0300

    refactor: Atualizando metodo de LONG para UUID

--- src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/dao/IDaoFuncaoTecnicoAdministrativo.java
+++ src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/dao/IDaoFuncaoTecnicoAdministrativo.java
@@ -2,6 +2,9 @@ package br.com.ifba.entity.funcaotecnicoadministrativo.dao;
 
 import br.com.ifba.entity.funcaotecnicoadministrativo.model.FuncaoTecnicoAdministrativo;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,14 @@ public interface IDaoFuncaoTecnicoAdministrativo extends JpaRepository<FuncaoTec
     boolean existsByNome(String nome);
     boolean existsById(Long id);
 
+           /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Encontra um FuncaoTecnicoAdministrativo pelo ID passado por parâmetro.
+     * @param id O ID do FuncaoTecnicoAdministrativo a ser encontrado.
+     * @return um objeto opcional do tipo FuncaoTecnicoAdministrativo,
+     * que será nulo caso o FuncaoTecnicoAdministrativo não exista na base de dados.
+     */
+    Optional<FuncaoTecnicoAdministrativo> findById(UUID id);
 }

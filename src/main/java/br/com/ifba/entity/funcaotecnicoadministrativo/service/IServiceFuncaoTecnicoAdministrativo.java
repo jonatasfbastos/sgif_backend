8fe8f17c5b01603fbf4e5d50331ae82e4a28e270
andesson reis <andessonreys@gmail.com>
2023-10-23 00:43:21 -0300

    refactor: Atalizando inteface

--- src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/service/IServiceFuncaoTecnicoAdministrativo.java
+++ src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/service/IServiceFuncaoTecnicoAdministrativo.java
@@ -1,12 +1,40 @@
 package br.com.ifba.entity.funcaotecnicoadministrativo.service;
 
-import br.com.ifba.entity.funcaotecnicoadministrativo.model.FuncaoTecnicoAdministrativo;
 import java.util.List;
+import java.util.UUID;
 
+import br.com.ifba.entity.funcaotecnicoadministrativo.dto.FuncaoTecnicoAdministrativoResponseDto;
+import br.com.ifba.entity.funcaotecnicoadministrativo.model.FuncaoTecnicoAdministrativo;
+
+/**
+ * Interface de serviço para Função Técnico Administrativo.
+ *
+ * @author unknown
+ * @since V1.0.1
+ * @Editado por Andesson Reis
+ */
 public interface IServiceFuncaoTecnicoAdministrativo {
 
-    FuncaoTecnicoAdministrativo saveFuncaoTecnicoAdm(FuncaoTecnicoAdministrativo funcaoTecnicoAdm);
-    String deleteFuncaoTecnicoAdm(Long id);
-    List<FuncaoTecnicoAdministrativo> getAllFuncoesTecnicoAdm();
+    /**
+     * Salva uma Função Técnico Administrativo na base de dados e retorna um objeto DTO com os dados resumidos da Função Técnico Administrativo salva.
+     *
+     * @param funcaoTecnicoAdm - A Função Técnico Administrativo que será salva na base de dados.
+     * @return um objeto DTO com os dados resumidos da Função Técnico Administrativo salva.
+     */
+    FuncaoTecnicoAdministrativoResponseDto saveFuncaoTecnicoAdm(FuncaoTecnicoAdministrativo funcaoTecnicoAdm);
 
+    /**
+     * Deleta uma Função Técnico Administrativo com base no ID.
+     *
+     * @param id - O ID da Função Técnico Administrativo a ser deletada.
+     * @return um objeto DTO com os dados da Função Técnico Administrativo deletada.
+     */
+    FuncaoTecnicoAdministrativoResponseDto deleteFuncaoTecnicoAdm(UUID id);
+
+    /**
+     * Obtém uma lista de todas as Funções Técnico Administrativo como objetos DTO.
+     *
+     * @return uma lista de objetos DTO representando as Funções Técnico Administrativo.
+     */
+    List<FuncaoTecnicoAdministrativoResponseDto> getAllFuncoesTecnicoAdm();
 }

6fdc863c42f5ba104d05d7c583db34d6048cfb93
andesson reis <andessonreys@gmail.com>
2023-10-22 22:20:26 -0300

    refactor: Atualizando metodo de salvar

--- src/main/java/br/com/ifba/entity/funcaoservidor/service/ServiceFuncaoServidor.java
+++ src/main/java/br/com/ifba/entity/funcaoservidor/service/ServiceFuncaoServidor.java
@@ -1,37 +1,59 @@
 package br.com.ifba.entity.funcaoservidor.service;
 
+import br.com.ifba.entity.empenho.dao.IDaoEmpenho;
 import br.com.ifba.entity.funcaoservidor.dao.IDaoFuncaoServidor;
+import br.com.ifba.entity.funcaoservidor.dto.FuncaoServidorResponseDto;
 import br.com.ifba.entity.funcaoservidor.model.FuncaoServidor;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 import br.com.ifba.entity.tecnicoadministrativo.dao.IDaoTecnicoAdministrativo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 /**
+ * Service que fornece operações relacionadas a Função de Servidor.
  *
  * @author Gusd
+ * @since V1.0.1
+ * @Editado por Andesson Reis
  */
 @Service
 public class ServiceFuncaoServidor implements IServiceFuncaoServidor {
 
-    private final static String FUNCAO_NULL = "A função do técnico está vazia, preencha e tente novamente.";
-    public final static String FUNCAO_NAO_EXISTE = "A função informada não existe.";
-    public final static String FUNCAO_INVÁLIDA = "Função inválida.";
-    public final static String FUNCAO_EXISTE = "Função já existe";
-    //mensagem de erro caso exista técnico administrativo atrelado a uma função
-    private final static String TECNICO_EXISTE = "Função não pode ser excluída, pois existe Técnico cadastrado";
+    // =========================================================== //
+    // =============== [ ATRIBUTOS ] ================ //
+    // =========================================================== //
 
     @Autowired
-    private IDaoFuncaoServidor daoFuncaoServidor;
+    private IDaoFuncaoServidor funcaoServidorDao;
+
+    @Autowired
     private IDaoTecnicoAdministrativo daoTecnicoAdministrativo;
 
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
+
+    // =========================================================== //
+    // =============== [ MÉTODOS ] ================== //
+    // =========================================================== //
+
+
+
+    /**
+     * Salva uma Função de Servidor na base de dados e retorna um objeto DTO com os dados resumidos da Função de Servidor salva.
+     *
+     * @param funcaoServidor - A Função de Servidor que será salva na base de dados.
+     * @return um objeto DTO com os dados resumidos da Função de Servidor salva.
+     * @author Andesson Reis
+     * @since V1.0.1
+     */
     @Override
-    public FuncaoServidor saveFuncaoServidor(FuncaoServidor funcaoServidor) {
-        if (funcaoServidor == null) {
-            throw new BusinessException(FUNCAO_NULL);
-        } else{
-            return daoFuncaoServidor.save(funcaoServidor);
-        }      
+    public FuncaoServidorResponseDto saveFuncaoServidor(FuncaoServidor funcaoServidor) {
+        
+        return objectMapperUtil.map(
+                funcaoServidorDao.save(funcaoServidor),
+                FuncaoServidorResponseDto.class);
     }
 
     @Override

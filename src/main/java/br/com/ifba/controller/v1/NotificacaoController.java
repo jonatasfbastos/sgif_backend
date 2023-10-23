ec08a1100aa00933edd56134da65a356e949a00b
andesson reis <andessonreys@gmail.com>
2023-10-23 09:50:55 -0300

    feat: Criado endpoint de 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/NotificacaoController.java
+++ src/main/java/br/com/ifba/controller/v1/NotificacaoController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -81,6 +86,19 @@ public class NotificacaoController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(notificacaoService.saveNotification(objectMapperUtil.map(notificacaoDto, Notificacao.class)));
+    }
+
+
+    /**
+     * Deleta uma notificação com base no seu ID.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/notificacoes/notificacao/{id}", consumes = "application.json")
+    public ResponseEntity<?> deletarNotificacaoPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(notificacaoService.deleteNotification(id));
     }
 
 }

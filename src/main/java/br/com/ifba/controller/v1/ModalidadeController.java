7a29fa4d685009de963d58ccb71a6dd609bc1622
andesson reis <andessonreys@gmail.com>
2023-10-23 12:58:06 -0300

    feat: Criado endpoit de 'UPDATE'

--- src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
+++ src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,17 +44,30 @@ public class ModalidadeController {
             .body(modalidadeService.getAllModalidade());
     }
 
-        /**
+    /**
      * Salva uma modalidade.
      *
      * @return Uma entidade de resposta genérica.
      */
     @PostMapping(consumes = "application/json")
     public ResponseEntity<?> salvarModalidade(@Valid @RequestBody ModalidadeRequestDto modalidadeDto, BindingResult result) {
-        
+
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(modalidadeService.saveModalidade(objectMapperUtil.map(modalidadeDto, Modalidade.class)));
     }
+
+    /**
+     * Atualiza uma modalidade.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(consumes = "application/json")
+    public ResponseEntity<?> atualizarModalidade(@Valid @RequestBody ModalidadeRequestDto modalidadeDto, BindingResult result) {
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(modalidadeService.updateModalidade(objectMapperUtil.map(modalidadeDto, Modalidade.class)));
+    }
+
 
 }

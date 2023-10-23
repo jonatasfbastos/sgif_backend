2b3ef074f319184ac97956e1461daa3cbbecf4b5
andesson reis <andessonreys@gmail.com>
2023-10-22 22:12:42 -0300

    refactor: Atualizando interface

--- src/main/java/br/com/ifba/entity/funcaoservidor/service/IServiceFuncaoServidor.java
+++ src/main/java/br/com/ifba/entity/funcaoservidor/service/IServiceFuncaoServidor.java
@@ -1,17 +1,51 @@
 package br.com.ifba.entity.funcaoservidor.service;
 
-import br.com.ifba.entity.funcaoservidor.model.FuncaoServidor;
-
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.ifba.entity.funcaoservidor.dto.FuncaoServidorResponseDto;
+import br.com.ifba.entity.funcaoservidor.model.FuncaoServidor;
+
+/**
+ * Interface de serviço para Função de Servidor.
+ *
+ * @author Gusd
+ * @since V1.0.1
+ * @Editado por Andesson Reis
+ */
 @Service
 public interface IServiceFuncaoServidor {
 
-    public abstract FuncaoServidor saveFuncaoServidor(FuncaoServidor funcaoServidor);
-    public abstract FuncaoServidor updateFuncaoServidor(FuncaoServidor funcaoServidor);
-    public abstract void deleteFuncaoServidor(FuncaoServidor funcaoServidor);
-    public abstract List<FuncaoServidor> getAllFuncaoServidor();
+    /**
+     * Salva uma Função de Servidor na base de dados e retorna um objeto DTO com os dados resumidos da Função de Servidor salva.
+     *
+     * @param funcaoServidor - A Função de Servidor que será salva na base de dados.
+     * @return um objeto DTO com os dados resumidos da Função de Servidor salva.
+     */
+    FuncaoServidorResponseDto saveFuncaoServidor(FuncaoServidor funcaoServidor);
 
+    /**
+     * Atualiza uma Função de Servidor na base de dados e retorna um objeto DTO com os dados resumidos da Função de Servidor atualizada.
+     *
+     * @param funcaoServidor - A Função de Servidor que será atualizada na base de dados.
+     * @return um objeto DTO com os dados resumidos da Função de Servidor atualizada.
+     */
+    FuncaoServidorResponseDto updateFuncaoServidor(FuncaoServidor funcaoServidor);
+
+    /**
+     * Deleta uma Função de Servidor com base no ID.
+     *
+     * @param id - O ID da Função de Servidor a ser deletada.
+     * @return um objeto DTO com os dados da Função de Servidor deletada.
+     */
+    FuncaoServidorResponseDto deleteFuncaoServidor(UUID id);
+
+    /**
+     * Obtém uma lista de todas as Funções de Servidor como objetos DTO.
+     *
+     * @return uma lista de objetos DTO representando as Funções de Servidor.
+     */
+    List<FuncaoServidorResponseDto> getAllFuncaoServidor();
 }

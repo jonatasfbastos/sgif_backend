d9b995c069aa3aa64b113515b065ee878664e3a4
andesson reis <andessonreys@gmail.com>
2023-10-23 16:06:05 -0300

    feat: Criado endpoit de get

--- src/main/java/br/com/ifba/controller/v1/MatrizCurricularController.java
+++ src/main/java/br/com/ifba/controller/v1/MatrizCurricularController.java
@@ -1,7 +1,14 @@
 package br.com.ifba.controller.v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifba.entity.matrizcurricular.service.IServiceMatrizCurricular;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 /**
  * @apiNote Endpoint criado desde a versão 1.0.1
@@ -14,5 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class MatrizCurricularController {
-    
+    @Autowired
+    IServiceMatrizCurricular matrizCurricularService;
+
+    @Autowired
+    ObjectMapperUtil objectMapperUtil;
+
+    /**
+     * Obtém a lista de todas as matrizes curriculares.
+     *
+     * @return Uma lista de matrizes curriculares ou uma resposta de erro em caso de falha.
+     */
+    @GetMapping(consumes = "application/json")
+    public ResponseEntity<?> getMatrizesCurriculares() {
+        
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(matrizCurricularService.getAllMatrizCurricular());
+    }
 }

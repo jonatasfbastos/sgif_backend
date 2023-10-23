628286241a418c1ee1778032b808bb848511718b
andesson reis <andessonreys@gmail.com>
2023-10-23 12:56:58 -0300

    feat: Criado endpoit de SAVE

--- src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
+++ src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.modalidade.dto.ModalidadeRequestDto;
+import br.com.ifba.entity.modalidade.model.Modalidade;
 import br.com.ifba.entity.modalidade.service.IServiceModalidade;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 /**
@@ -30,9 +38,22 @@ public class ModalidadeController {
      */
     @GetMapping(consumes = "application/json")
     public ResponseEntity<?> getModalidades() {
-        
+
         return ResponseEntity.status(HttpStatus.OK)
             .body(modalidadeService.getAllModalidade());
+    }
+
+        /**
+     * Salva uma modalidade.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<?> salvarModalidade(@Valid @RequestBody ModalidadeRequestDto modalidadeDto, BindingResult result) {
+        
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(modalidadeService.saveModalidade(objectMapperUtil.map(modalidadeDto, Modalidade.class)));
     }
 
 }

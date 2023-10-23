82efe3a797bfcee7eaaaf8f833a00459a04992b1
andesson reis <andessonreys@gmail.com>
2023-10-23 16:07:10 -0300

    feat: Criado endpoit de 'SAVE'

--- src/main/java/br/com/ifba/controller/v1/MatrizCurricularController.java
+++ src/main/java/br/com/ifba/controller/v1/MatrizCurricularController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.matrizcurricular.dto.MatrizCurricularRequestDto;
+import br.com.ifba.entity.matrizcurricular.model.MatrizCurricular;
 import br.com.ifba.entity.matrizcurricular.service.IServiceMatrizCurricular;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -34,8 +42,21 @@ public class MatrizCurricularController {
      */
     @GetMapping(consumes = "application/json")
     public ResponseEntity<?> getMatrizesCurriculares() {
-        
+
         return ResponseEntity.status(HttpStatus.OK)
             .body(matrizCurricularService.getAllMatrizCurricular());
+    }
+
+    /**
+     * Salva uma matriz curricular.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<?> salvarMatrizCurricular(@Valid @RequestBody MatrizCurricularRequestDto matrizCurricularDto, BindingResult result) {
+        
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(matrizCurricularService.saveMatrizCurricular(objectMapperUtil.map(matrizCurricularDto, MatrizCurricular.class)));
     }
 }

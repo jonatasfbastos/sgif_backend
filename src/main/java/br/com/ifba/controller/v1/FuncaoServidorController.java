69878d319cd6111b7ad60e2cbe2c3f775a044e50
andesson reis <andessonreys@gmail.com>
2023-10-22 22:40:57 -0300

    feat: Criado endpoint de SAVE

--- src/main/java/br/com/ifba/controller/v1/FuncaoServidorController.java
+++ src/main/java/br/com/ifba/controller/v1/FuncaoServidorController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.funcaoservidor.dto.FuncaoServidorRequestDto;
+import br.com.ifba.entity.funcaoservidor.model.FuncaoServidor;
 import br.com.ifba.entity.funcaoservidor.service.IServiceFuncaoServidor;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -44,10 +52,24 @@ public class FuncaoServidorController {
      *
      * @return Uma lista de funções de servidor ou uma resposta de erro em caso de falha.
      */
-    @GetMapping(path = "/funcoes_servidor", consumes = "application/json")
+    @GetMapping(path = "/funcoesServidor", consumes = "application/json")
     public ResponseEntity<?> getFuncoesServidor() {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(funcaoServidorService.getAllFuncaoServidor());
     }
+
+    /**
+     * Salva uma função de servidor.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @PostMapping(path = "/funcoesServidor/funcaoServidor", consumes = "application/json")
+    public ResponseEntity<?> salvarFuncaoServidor(@Valid @RequestBody FuncaoServidorRequestDto funcaoServidorDto, BindingResult result) {
+
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.CREATED).body(funcaoServidorService.saveFuncaoServidor(objectMapperUtil.map(funcaoServidorDto, FuncaoServidor.class)));
+    }
+
 }

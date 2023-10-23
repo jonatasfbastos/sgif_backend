9d7fd8bc0073bafbc6e2d683f362aef6aebe856e
andesson reis <andessonreys@gmail.com>
2023-10-23 00:31:14 -0300

    refactor: Atualizando entidade

--- src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/model/FuncaoTecnicoAdministrativo.java
+++ src/main/java/br/com/ifba/entity/funcaotecnicoadministrativo/model/FuncaoTecnicoAdministrativo.java
@@ -10,19 +10,40 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import java.io.Serializable;
 import java.util.List;
 
-@Entity
+/**
+ * Esta classe representa uma função de técnico administrativo.
+ *
+ * @author unknown
+ * @Editado por Andesson Reis
+ * @since V1.0.1
+ * 
+ * Esta classe representa uma função de técnico administrativo da organização.
+ * Nenhum dos atributos desta classe pode ser nulo.
+ * 
+ * Veja também: {@link br.com.ifba.entity.tecnicoadministrativo.model.TecnicoAdministrativo}
+ */
+@Entity(name = "funcao_tecnico_administrativo")
+@Table(name = "funcoes_tecnico_administrativo")
 @Data
-@Table(name = "funcao_tecnico_administrativo")
 @EqualsAndHashCode(callSuper = false)
 public class FuncaoTecnicoAdministrativo extends PersistenceEntity implements Serializable {
 
+    /**
+     * O nome da função de técnico administrativo. Não pode ser nulo.
+     */
+    @NotNull
     private String nome;
+
+    /**
+     * A descrição da função de técnico administrativo.
+     */
     private String descricao;
     
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "funcaoTecnicoAdministrativo")
     private List<TecnicoAdministrativo> tecnicoAdministrativos;
-
 }

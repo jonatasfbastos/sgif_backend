ea44b735b2ec1439b390ce9e74b7b895f6f32d15
andesson reis <andessonreys@gmail.com>
2023-10-23 12:58:53 -0300

    feat: Criado endpoit de 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
+++ src/main/java/br/com/ifba/controller/v1/ModalidadeController.java
@@ -1,12 +1,16 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,6 +71,18 @@ public class ModalidadeController {
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.OK).body(modalidadeService.updateModalidade(objectMapperUtil.map(modalidadeDto, Modalidade.class)));
+    }
+
+    /**
+     * Deleta uma modalidade com base no seu ID.
+     *
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarModalidadePorID(@Valid @PathVariable("id") UUID id) {
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+            .body(modalidadeService.deleteModalidade(id));
     }
 
 

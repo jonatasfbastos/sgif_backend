f3c4a992c1e9e851ee627662745148dd612d60a6
andesson reis <andessonreys@gmail.com>
2023-10-23 09:56:26 -0300

    refactor: Atualizando entidade

--- src/main/java/br/com/ifba/entity/mensagem/model/Mensagem.java
+++ src/main/java/br/com/ifba/entity/mensagem/model/Mensagem.java
@@ -1,34 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.entity.mensagem.model;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
-import java.io.Serializable;
-import java.util.Date;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import lombok.Data;
+import javax.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
+ * Esta classe representa uma mensagem.
  *
  * @author felipe
+ * @Editado por Andesson Reis
+ * @since V1.0.1
+ * 
+ * Esta classe representa uma mensagem da aplicação.
+ * Nenhum dos atributos desta classe pode ser nulo.
  */
-
-@Entity
-@Table(name = "mensagem")
+@Entity(name = "mensagem")
+@Table(name = "mensagens")
 @Data
-public class Mensagem extends PersistenceEntity implements Serializable{
+@EqualsAndHashCode(callSuper = false)
+public class Mensagem extends PersistenceEntity implements Serializable {
+
+    /**
+     * O nome da mensagem. Não pode ser nulo.
+     */
+    @NotNull
     private String nome;
+
+    /**
+     * A descrição da mensagem. Não pode ser nula.
+     */
+    @NotNull
     private String descricao;
-    
+
+    /**
+     * A data de início da mensagem. Não pode ser nula.
+     */
+    @NotNull
     @Temporal(TemporalType.DATE)
     private Date dataInicio;
-    
+
+    /**
+     * A data de término da mensagem. Não pode ser nula.
+     */
+    @NotNull
     @Temporal(TemporalType.DATE)
     private Date dataFim;
-
 }

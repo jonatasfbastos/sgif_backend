9c6779a20ee5ba92bd79096370b84630439be265
andesson reis <andessonreys@gmail.com>
2023-10-25 10:24:51 -0300

    feat: Criado endpoint para 'DELETE'

--- src/main/java/br/com/ifba/controller/v1/TipoTurmaController.java
+++ src/main/java/br/com/ifba/controller/v1/TipoTurmaController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +24,12 @@ import br.com.ifba.entity.tipoturma.service.IServiceTipoTurma;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 /**
+ * @apiNote Endpoint criado desde a versão 1.0.1
+ *
+ * Controller para gerenciar Tipos de Turma.
+ *
  * @author Andesson reis
- * Desde V1.0.1
+ * @since Desde V1.0.1
  * 
  *Veja também: {@link br.com.ifba.entity.tipoturma.model.TipoTurma}
  */
@@ -81,12 +90,27 @@ public class TipoTurmaController {
      * @param result - Resultado da validação.
      * @return Uma entidade de resposta genérica.
      */
-    @PostMapping(path = "/tipoTurma/tipoTurma", consumes = "application/json")
+    @PostMapping(path = "/tiposTurma/tipoTurma", consumes = "application/json")
     public ResponseEntity<?> salvarTipoTurma(@Valid @RequestBody TipoTurma tipoTurmaDto, BindingResult result) {
 
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(tipoTurmaService.saveTipoTurma(objectMapperUtil.map(tipoTurmaDto, TipoTurma.class)));
+    }
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     * 
+     * Deleta um Tipo de Turma com base no seu ID.
+     *
+     * @param id - O ID do Tipo de Turma a ser deletado.
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/tiposTurma/tipoTurma/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarTipoTurmaPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(tipoTurmaService.deleteTipoTurma(id));
     }
 
 }

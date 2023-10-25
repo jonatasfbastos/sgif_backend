a615a9535f32691496acfb0d0d5180953bf76d91
NekoYasha <autornekoyasha@gmail.com>
2023-10-25 09:59:58 -0300

    📜 feat: Criando controller para a entidade Disciplina

---
+++ src/main/java/br/com/ifba/controller/v1/DisciplinaController.java
@@ -0,0 +1,16 @@
+package br.com.ifba.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Giovane Neves
+ * @since Desde V1.0.1
+ */
+@RestController
+@RequestMapping(path = "/apif/v1")
+public class DisciplinaController {
+
+    // TODO: Criar CRUD no controller
+
+}

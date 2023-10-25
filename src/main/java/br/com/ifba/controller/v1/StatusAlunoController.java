cdae6d4bee0425440264d691be6a373a44c33952
andesson reis <andessonreys@gmail.com>
2023-10-25 12:02:58 -0300

    feat: Criado endpoint para update

--- src/main/java/br/com/ifba/controller/v1/StatusAlunoController.java
+++ src/main/java/br/com/ifba/controller/v1/StatusAlunoController.java
@@ -1,12 +1,20 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.statusaluno.dto.StatusAlunoRequestDto;
+import br.com.ifba.entity.statusaluno.model.StatusAluno;
 import br.com.ifba.entity.statusaluno.service.IServiceStatusAluno;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -36,9 +44,23 @@ public class StatusAlunoController {
      */
     @GetMapping(path = "/statusAlunos", consumes = "application/json")
     public ResponseEntity<?> getStatusAlunos() {
-        
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(statusAlunoService.getAllstatus());
+    }
+
+        /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     * Atualiza um Status do Aluno.
+     * 
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(path = "/statusAlunos/statusAluno", consumes = "application/json")
+    public ResponseEntity<?> atualizarStatusAluno(@Valid @RequestBody StatusAlunoRequestDto statusAlunoDto, BindingResult result) {
+        
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(statusAlunoService.updateStatus(objectMapperUtil.map(statusAlunoDto, StatusAluno.class)));
     }
     
 }

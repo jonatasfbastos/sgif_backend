bee432ed57ba4ee2a013d27872a7d565ffe1ba18
andesson reis <andessonreys@gmail.com>
2023-10-25 10:43:36 -0300

    refactor: Atualizando metodo de LONG para UUID

--- src/main/java/br/com/ifba/entity/statusaluno/dao/IDaoStatusAluno.java
+++ src/main/java/br/com/ifba/entity/statusaluno/dao/IDaoStatusAluno.java
@@ -6,6 +6,10 @@
 package br.com.ifba.entity.statusaluno.dao;
 
 import br.com.ifba.entity.statusaluno.model.StatusAluno;
+
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,5 +17,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Ellen Cristina
  */
 public interface IDaoStatusAluno extends JpaRepository<StatusAluno, Long> {
-    
+        /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Verifica se existe um StatusAluno com o ID passado por parâmetro
+     * na base de dados.
+     * @param id O id do StatusAluno a ser consultado.
+     * @return 'true' caso haja um StatusAluno com o Id
+     * passado por parâmetro na base de dados,
+     * 'false' caso contrário.
+     */
+    boolean existsById(UUID id);
+
+    /**
+     * @author Andesson Reis
+     * @since Desde V1.0.1
+     * 
+     * Encontra uma StatusAluno pelo ID passado por parâmetro.
+     * @param id O ID da StatusAluno a ser encontrado.
+     * @return um objeto opcional do tipo StatusAluno,
+     * que será nulo caso o requisição não exista na base de dados.
+     */
+    Optional<StatusAluno> findById(UUID id);
 }

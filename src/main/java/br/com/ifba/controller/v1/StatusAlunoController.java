334a3df4609bbb1e12d3a924005ef4e774a0cb8d
andesson reis <andessonreys@gmail.com>
2023-10-25 12:08:43 -0300

    feat: Criado endpoint para delete

--- src/main/java/br/com/ifba/controller/v1/StatusAlunoController.java
+++ src/main/java/br/com/ifba/controller/v1/StatusAlunoController.java
@@ -1,12 +1,17 @@
 package br.com.ifba.controller.v1;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -72,10 +77,23 @@ public class StatusAlunoController {
      */
     @PostMapping(path = "/statusAlunos/statusAluno", consumes = "application/json")
     public ResponseEntity<?> salvarStatusAluno(@Valid @RequestBody StatusAlunoRequestDto statusAlunoDto, BindingResult result) {
-        
+
         return result.hasErrors()
                 ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
                 : ResponseEntity.status(HttpStatus.CREATED).body(statusAlunoService.saveStatus(objectMapperUtil.map(statusAlunoDto, StatusAluno.class)));
+    }
+
+    /**
+     * @apiNote Endpoint criado desde a versão 1.0.1
+     * Deleta um Status do Aluno com base no ID.
+     * 
+     * @return Uma entidade de resposta genérica.
+     */
+    @DeleteMapping(path = "/statusAlunos/statusAluno/{id}", consumes = "application/json")
+    public ResponseEntity<?> deletarStatusAlunoPorID(@Valid @PathVariable("id") @NotNull UUID id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(statusAlunoService.deleteStatus(id));
     }
     
 }

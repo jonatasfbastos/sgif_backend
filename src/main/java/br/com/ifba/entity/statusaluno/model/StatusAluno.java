a5e3d24b903f569fe239ff7f06f3e643ed7718df
andesson reis <andessonreys@gmail.com>
2023-10-25 10:39:14 -0300

    refactor: Atualizando entidade

--- src/main/java/br/com/ifba/entity/statusaluno/model/StatusAluno.java
+++ src/main/java/br/com/ifba/entity/statusaluno/model/StatusAluno.java
@@ -1,35 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.ifba.entity.statusaluno.model;
 
 import br.com.ifba.entity.aluno.model.Aluno;
 import br.com.ifba.infrastructure.model.PersistenceEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.io.Serializable;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
 /**
+ * Esta classe representa o status de um aluno.
  *
  * @author Ellen Cristina
+ * @since V1.0.1
+ * Editado por Andesson Reis
  */
-@Entity
-@Table(name = "statusaluno")
+@Entity(name = "status_aluno")
 @Data
+@Table(name = "status_aluno")
 @EqualsAndHashCode(callSuper = false)
-public class StatusAluno extends PersistenceEntity implements Serializable{
+public class StatusAluno extends PersistenceEntity implements Serializable {
+
+    /**
+     * O nome do status do aluno.
+     */
     private String nome;
+
+    /**
+     * A descrição do status do aluno.
+     */
     private String descricao;
-    
+
     @JsonIgnore
-    @OneToMany(mappedBy = "statusAluno", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "statusAluno")
     private List<Aluno> alunos;
 }

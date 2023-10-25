7a7130d4e6e0cc5e8e63aa4862bbdb1195764df0
andesson reis <andessonreys@gmail.com>
2023-10-25 11:30:55 -0300

    refactor: Atualizando metodo do salvar

--- src/main/java/br/com/ifba/entity/statusaluno/service/ServiceStatusAluno.java
+++ src/main/java/br/com/ifba/entity/statusaluno/service/ServiceStatusAluno.java
@@ -1,51 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.ifba.entity.statusaluno.service;
 
 import br.com.ifba.entity.aluno.dao.IDaoAluno;
 import br.com.ifba.entity.statusaluno.dao.IDaoStatusAluno;
+import br.com.ifba.entity.statusaluno.dto.StatusAlunoResponseDto;
 import br.com.ifba.entity.statusaluno.model.StatusAluno;
 import br.com.ifba.infrastructure.exception.BusinessException;
+import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
+ * Service que fornece operações relacionadas ao Status dos Alunos.
  *
  * @author Ellen Cristina
+ * Editado por Andesson Reisa
+ * @since V1.0.1
  */
 @Service
 public class ServiceStatusAluno implements IServiceStatusAluno{
-     // Mensagem de erro se o Status for null.
-    public final static String STATUS_NULL = "Dados do Status nao preenchidos";
-    // Mensagem de erro se o Status ja¡ existir.
-    public final static String STATUS_EXISTE = "Status ja existente no Banco de dados";
-    // Mensagem de erro se o Status nao existir no banco.
-    public final static String STATUS_NAO_EXISTE = "Status nao existente no Banco de dados";
-    // Mensagem de erro se o Status for invalido.
-    public final static String STATUS_INVALIDO = "As informacoes do Status nao sao validas";
-    //mensagem de erro caso exista aluno atrelado a um status
-    private final static String ALUNO_EXISTE = "Status não pode ser excluído, pois existe Aluno cadastrado";
-    
-     //================= OBJETO =================================================
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+
     @Autowired
-    private IDaoStatusAluno statusDao;
-    private IDaoAluno alunoDao;
+    private IDaoStatusAluno statusAlunoDao;
 
-     //================= METODOS ================================================
+    @Autowired
+    private ObjectMapperUtil objectMapperUtil;
 
+    // =========================================================== //
+    // =============== [        MÉTODOS       ] ================== //
+    // =========================================================== /
+
+
+    /**
+     * Salva o Status do Aluno na base de dados e retorna um objeto DTO com os dados resumidos do Status do Aluno salvo.
+     *
+     * @param status - O Status do Aluno que será salvo na base de dados.
+     * @return um objeto DTO com os dados resumidos do Status do Aluno salvo.
+     * @author Andesson Reis
+     * @since V1.0.1
+     */
     @Override
-    public StatusAluno saveStatus(StatusAluno status) {
-         if(status == null) {
-            throw new BusinessException(STATUS_NULL);
-       } else{
-            return statusDao.save(status);
-       }
+    public StatusAlunoResponseDto saveStatus(StatusAluno status) {
+        
+        return objectMapperUtil.map(
+                statusAlunoDao.save(status),
+                StatusAlunoResponseDto.class);
     }
+
 
     @Override
     public StatusAluno updateStatus(StatusAluno status) {

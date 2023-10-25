a668a81500a2ab9a92140e1317a2c0c9ee094d57
andesson reis <andessonreys@gmail.com>
2023-10-25 10:18:13 -0300

    feat: Criado endpoint para 'update'

--- src/main/java/br/com/ifba/controller/v1/TipoTurmaController.java
+++ src/main/java/br/com/ifba/controller/v1/TipoTurmaController.java
@@ -1,12 +1,19 @@
 package br.com.ifba.controller.v1;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifba.controller.v1.util.ResultError;
+import br.com.ifba.entity.tipoturma.model.TipoTurma;
 import br.com.ifba.entity.tipoturma.service.IServiceTipoTurma;
 import br.com.ifba.infrastructure.util.ObjectMapperUtil;
 
@@ -43,5 +50,20 @@ public class TipoTurmaController {
         
         return ResponseEntity.status(HttpStatus.OK)
                 .body(tipoTurmaService.getAllTipoTurma());
+    }
+
+        /**
+     * Atualiza um Tipo de Turma.
+     *
+     * @param tipoTurmaDto - O Tipo de Turma a ser atualizado.
+     * @param result - Resultado da validação.
+     * @return Uma entidade de resposta genérica.
+     */
+    @PutMapping(path = "/tipoTurma/tipoTurma", consumes = "application/json")
+    public ResponseEntity<?> atualizarTipoTurma(@Valid @RequestBody TipoTurma tipoTurmaDto, BindingResult result) {
+        
+        return result.hasErrors()
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK).body(tipoTurmaService.updateTipoTurma(objectMapperUtil.map(tipoTurmaDto, TipoTurma.class)));
     }
 }

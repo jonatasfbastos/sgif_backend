ecd33d4703f0badc70c0aef1541f36951d1cceea
NekoYasha <autornekoyasha@gmail.com>
2023-11-02 13:45:17 -0300

    📜 feat: Adicionando métodos da interface 'UserDetails

--- src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
+++ src/main/java/br/com/ifba/entity/usuario/model/Usuario.java
@@ -8,7 +8,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import br.com.ifba.entity.usuario.dto.UsuarioSimpleResponseDto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
@@ -17,16 +16,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
- * @author vitor
- * Editado por Andesson Reis
- * @since V1.0.1
- * 
  * Esta classe, descendente da classe Pessoa, representa um usuario.
- * Nenhum dos atributos desta classes pode ser nulo.
- * 
- *  Veja também: {@link br.com.ifba.entity.perfilusuario}
+ * Nenhum dos atributos desta classe pode ser nulo.
+ *
+ * @author vitor
+ * Editado por Andesson Reis, Giovane Neves
+ * @since Desde V1.0.1
+ *
+ *  Veja também: {@link br.com.ifba.entity.perfilusuario.model.PerfilUsuario}
  */
 
 @Entity(name = "usuario")
@@ -35,7 +40,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class Usuario extends PersistenceEntity {
+public class Usuario extends PersistenceEntity implements UserDetails {
 
     // =========================================================== //
     // =============== [        ATRIBUTOS       ] ================ //
@@ -69,19 +74,91 @@ public class Usuario extends PersistenceEntity {
     // =========================================================== //
 
     /**
+     * Pega as permissões do usuário.
+     *
      * @author Giovane Neves
-     * Desde V1.0.1
-     *
-     * Cria um objeto de resposta DTO simples para ser enviado no ResponseBody
-     * das requisições direcionada à entidade 'Usuario'.
-     *
-     * @return um objeto UsuarioSimpleResponseDto com dados do formulário.
+     * @since Desde V1.0.1
+     * @return lista de permissões do usuário.
      */
-    public UsuarioSimpleResponseDto toSimpleResponseDto(){
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
 
-         return new UsuarioSimpleResponseDto(this.login);
+        // TODO: Adicionar lógica para dar permissão de administrador a determinados usuários.
 
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
-    
 
+    /**
+     * Pega senha do usuário.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return senha do usuário.
+     */
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    /**
+     * Pega login do usuário.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return login do usuário.
+     */
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    // TODO: Trabalhar com os métodos abaixo para ativação e desativação de contas.
+
+    /**
+     * Retorna status da conta (conta que expira ou não).
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return status da conta.
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * Retorna status da conta do usuário (conta do usuário bloqueável ou não).
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return status da conta do usuário.
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * Retorna status das credenciais (credenciais que expiram ou não).
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return status das credenciais.
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * Retorna status do usuário (ativado ou desativado).
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @return status do usuário.
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

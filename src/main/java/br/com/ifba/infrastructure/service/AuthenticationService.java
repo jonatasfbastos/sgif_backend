e42a907042aac3eaa2586473b618955b95dfdbda
NekoYasha <autornekoyasha@gmail.com>
2023-11-02 13:37:06 -0300

    📜 feat: Criando service para autenticação de usuário

--- src/main/java/br/com/ifba/infrastructure/service/AuthenticationService.java
+++ src/main/java/br/com/ifba/infrastructure/service/AuthenticationService.java
@@ -1,2 +1,35 @@
-package br.com.ifba.infrastructure.service;public class AuthenticationService {
+package br.com.ifba.infrastructure.service;
+
+import br.com.ifba.entity.usuario.dao.IDaoUsuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Giovane Neves
+ * @since Desde V1.0.1
+ */
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    IDaoUsuario usuarioDao;
+
+    /**
+     * Carrega um usuário da base de dados pelo login passado por parâmetro.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @param login O login do usuário a ser buscado.
+     * @return dados do usuário com o login passo passado por parâmetro.
+     * @throws UsernameNotFoundException caso não exista um usuário com o
+     * login passado por parâmetro na base de dados.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return usuarioDao.findByLogin(login).get();
+    }
 }

d50104715f9dca3025b0671f9a3b186ad4ca1de9
NekoYasha <autornekoyasha@gmail.com>
2023-11-02 14:02:58 -0300

    📜feat: Criando service para geração de token

--- src/main/java/br/com/ifba/infrastructure/service/TokenService.java
+++ src/main/java/br/com/ifba/infrastructure/service/TokenService.java
@@ -1,2 +1,36 @@
-package br.com.ifba.infrastructure.service;public class TokenService {
+package br.com.ifba.infrastructure.service;
+
+import br.com.ifba.entity.usuario.model.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Giovane Neves
+ * @since Desde V1.0.1
+ */
+@Service
+public class TokenService {
+
+    @Value(value = "${chave_secreta}")
+    private String CHAVE_SECRETA;
+
+    /**
+     * Gera um token para o usuário autenticado.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @param usuario O usuário autenticado.
+     * @return um token AWT para o usuário autenticado.
+     */
+    public String gerarToken(Usuario usuario){
+
+        return Jwts.builder()
+                .setSubject(usuario.getLogin())
+                .signWith(SignatureAlgorithm.HS512, CHAVE_SECRETA)
+                .compact();
+    }
+
 }

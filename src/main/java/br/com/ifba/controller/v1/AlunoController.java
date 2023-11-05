9d44853101c022583aa91756e9c4c34094aeef92
NekoYasha <autornekoyasha@gmail.com>
2023-11-05 11:10:25 -0300

    📦 chore: Adicionando anotação 'CrossOrigin'

--- src/main/java/br/com/ifba/controller/v1/AlunoController.java
+++ src/main/java/br/com/ifba/controller/v1/AlunoController.java
@@ -10,14 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -29,6 +22,7 @@ import java.util.UUID;
  * @since Desde V1.0.1
  */
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/apif/v1")
 public class AlunoController {
 

4d532d2014e80ceaec66a30d507f288fb191eb61
NekoYasha <autornekoyasha@gmail.com>
2023-11-10 10:05:30 -0300

    📜 feat: Criando configuração de hierarquia de papéis

--- src/main/java/br/com/ifba/config/security/RoleHierarchyConfig.java
+++ src/main/java/br/com/ifba/config/security/RoleHierarchyConfig.java
@@ -3,10 +3,42 @@ package br.com.ifba.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+
+/**
+ * @author Giovane Neves
+ * @since Desde V1.0.1
+ */
 @Configuration
-public class RoleHierarchy {
+public class RoleHierarchyConfig {
 
     @Bean
-    public RoleH
+    static RoleHierarchy roleHierarchy(){
+
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy(
+                "ROLE_ADMIN > ROLE_STAFF\n".concat(
+                        "ROLE_STAFF > ROLE_PROFESSOR\n".concat(
+                                "ROLE_PROFESSOR > ROLE_USER"
+                        )
+                )
+        );
+
+        return hierarchy;
+
+    }
+
+    @Bean
+    static MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+
+        return expressionHandler;
+    }
 
 }

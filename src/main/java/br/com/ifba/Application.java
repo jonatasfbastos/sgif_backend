19a8d2a90408b3c1cfda6428f9c19fc2f1865bc8
NekoYasha <autornekoyasha@gmail.com>
2023-11-16 09:21:16 -0300

    🧹 clear: Retirando imports não utlizados

--- src/main/java/br/com/ifba/Application.java
+++ src/main/java/br/com/ifba/Application.java
@@ -3,9 +3,6 @@ package br.com.ifba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication

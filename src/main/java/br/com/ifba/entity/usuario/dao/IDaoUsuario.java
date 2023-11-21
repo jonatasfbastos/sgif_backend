f40b1340d213bb68913de05e2de18c86367a2781
NekoYasha <autornekoyasha@gmail.com>
2023-11-21 09:23:22 -0300

    📜 feat: Adicionando assinatura do método 'findUserByLogin'

--- src/main/java/br/com/ifba/entity/usuario/dao/IDaoUsuario.java
+++ src/main/java/br/com/ifba/entity/usuario/dao/IDaoUsuario.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ifba.entity.usuario.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -21,4 +22,15 @@ public interface IDaoUsuario extends JpaRepository<Usuario, UUID> {
     Optional<Usuario> findById(UUID id);
     Optional<Usuario> findByLoginAndSenha(String login, String senha);
     Optional<Usuario> findByLogin(String login);
+
+    /**
+     * Encontra um usuário pelo login passado por parâmetro.
+     *
+     * @author Giovane Neves
+     * @since V1.0.1
+     * @param login O login do usuário a ser buscado.
+     * @return instância com dados do usuário encontrado.
+     */
+    @Query("select u from Usuario u where u.login = ?1")
+    Usuario findUserByLogin(String login);
 }

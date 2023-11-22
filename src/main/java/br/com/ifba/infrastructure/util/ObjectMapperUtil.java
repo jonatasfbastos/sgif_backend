9a3c0ea617b3c4290cd7e8cbd71f7557c81b095a
NekoYasha <autornekoyasha@gmail.com>
2023-11-22 13:05:49 -0300

    📜 feat: Criando método 'mapDto'

--- src/main/java/br/com/ifba/infrastructure/util/ObjectMapperUtil.java
+++ src/main/java/br/com/ifba/infrastructure/util/ObjectMapperUtil.java
@@ -1,8 +1,11 @@
 package br.com.ifba.infrastructure.util;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Function;
 import java.util.List;
@@ -16,7 +19,17 @@ import java.util.List;
 @Component
 public class ObjectMapperUtil {
 
+
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+
     private static final ModelMapper MODEL_MAPPER;
+
+
+    // =========================================================== //
+    // =============== [         MÉTODOS      ] ================== //
+    // =========================================================== //
 
     /**
      * Bloco de código estático para instanciar o ModelMapper
@@ -38,8 +51,88 @@ public class ObjectMapperUtil {
      * @param <Output> O tipo de saída.
      */
     public <Input, Output> Output map(final Input object, final Class<Output> clazz){
+        Output c =  MODEL_MAPPER.map(object, clazz);
 
-        return MODEL_MAPPER.map(object, clazz);
+        return c;
+
+    }
+
+    /**
+     * Copia dados de uma origem para um objeto de destino.
+     *
+     * @author Giovane Neves.
+     * @since V1.0.1
+     * @param s O objeto de origem.
+     * @param t O objeto de destino.
+     * @return instância do objeto de destino com dados do objeto de origem.
+     * @param <Source> O tipo de origem.
+     * @param <Target> O tipo de saída.
+     */
+    public <Source, Target> Target mapDto(final Source s, Target t){
+
+        try{
+
+            // Criando Foreach na lista de atributos da classe Source.
+            for(Field sourceField : s.getClass().getDeclaredFields()){
+
+                boolean fieldExists = Arrays.stream(t.getClass().getDeclaredFields())
+                        .anyMatch(f -> f.getName().equals(sourceField.getName()));
+
+                // Salta a iteração caso ambas as classes não possuem o atributo com o mesmo nome.
+                if(!fieldExists)
+                    continue;
+
+                // Pega o campo do Target que receberá o valor do campo com o mesmo nome em Source.
+                Field targetField = t.getClass().getDeclaredField(sourceField.getName());
+
+                // Verifica se o atributo atual de Source é um Record.
+                if(isRecord(sourceField.getClass())){
+
+
+                    sourceField.setAccessible(true);
+                    Object sourceAggregateObject = sourceField.get(s);
+
+
+                    targetField.setAccessible(true);
+
+                    // Copia os atributos do Record para os atributos do objeto agregado de mesmo nome com uma chamada recursiva.
+                    targetField.set(t, mapDto(sourceAggregateObject, targetField.getType()));
+
+                    continue;
+                }
+
+                sourceField.setAccessible(true);
+                targetField.setAccessible(true);
+
+                // Atribui o valor do atributo atual de source para o atributo de mesmo nome em Target.
+                Object value = sourceField.get(s);
+
+                targetField.set(t, value);
+
+            }
+
+        } catch(Exception ex){
+
+            ex.printStackTrace();
+
+        }
+
+
+        return t;
+
+    }
+
+    /**
+     * Verifica se uma classe é um record ou não.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @param clazz A classe a ser verificada.
+     * @return 'true' caso a classe seja um record, 'false' caso contrário.
+     */
+    private boolean isRecord(Class<?> clazz){
+
+        return clazz.isRecord();
 
     }
 

a1383dd67bf7eb52f34ba804c3683819d40ad783
NekoYasha <autornekoyasha@gmail.com>
2023-11-21 21:55:09 -0300

    ✏ refactor: Editando classe

---
+++ src/main/java/br/com/ifba/config/security/JWTApiAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package br.com.ifba.config.security;
+
+public class JWTApiAuthenticationFilter {
+}
--- src/main/java/br/com/ifba/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/ifba/config/security/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package br.com.ifba.config.security;
 
+import br.com.ifba.ApplicationContextLoad;
+import br.com.ifba.infrastructure.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.HttpMethod;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,8 +20,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpSessionListener;
 
 /**
  * @author Giovane Neves
@@ -24,52 +35,90 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig implements WebMvcConfigurer {
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer, HttpSessionListener {
 
 
-    // TODO: Limpar essa classe
+    // =========================================================== //
+    // =============== [        ATRIBUTOS       ] ================ //
+    // =========================================================== //
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    // =========================================================== //
+    // =============== [         ENDPOINTS      ] ================ //
+    // =========================================================== //
+
+    /**
+     * Configura construtor de gerenciador de autenticação.
+     *
+     * @author Giovane Neves
+     * @since Desde V1.0.1
+     * @param auth O construtor de gerenciador de autenticação.
+     * @throws Exception caso haja algum erro na configuração.
+     */
     @Bean
-    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
-        http.csrf().disable();
-
-        http.authorizeHttpRequests((auth) ->
-                auth.antMatchers(HttpMethod.GET)
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/apif/v1/login").permitAll()
-                .anyRequest().authenticated());
-
-        return http.build();
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
 
     }
+
     /**
-     * Define filtros da cadeia de segurança.
+     * Cofigura segurança da aplicação.
      *
      * @author Giovane Neves
      * @since Desde V1.0.1
      * @param http O objeto http.
      * @return configurações da cadeia de segurança.
      */
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        // TODO:Rever privacidade de alguns endpoints.
+        // TODO: Rever privacidade de endpoints POST.
 
-        return http.csrf()
-                    .disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET) // Todos os métodos GET estão públicos
-                    .permitAll()
-                .antMatchers(HttpMethod.POST, "/apif/v1/login")
-                    .permitAll()
-                .anyRequest()
-                    .authenticated()
-                .and()
-                .build();
-    }*/
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .disable()
+                .authorizeRequests((auth) -> {
+                    auth.antMatchers(HttpMethod.GET).permitAll();
+                    auth.antMatchers(HttpMethod.POST, "/apif/v1/usuarios/usuario").permitAll();
+                    auth.antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    auth.anyRequest().authenticated();
+
+                    // Configuração de Logout.
+                    try {
+
+                        auth.and().logout().logoutSuccessUrl("PATH_TO_LOGOUT_SCREEN");
+                        auth.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // Configuração de JWT.
+
+                    try {
+
+                        // TODO: Resolver erro no authenticationManager().
+                        auth.and().addFilterAfter(new JWTLoginFilter(
+                                "/login",
+                                        ApplicationContextLoad.getApplicationContext().getBean(AuthenticationManager.class)),
+                                UsernamePasswordAuthenticationFilter.class
+                        );
+
+                        auth.and().addFilterBefore(new JWTApiAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                });
+
+        return http.build();
+
+    }
 
     /**
      * Define configurações de CORS.
@@ -84,35 +133,5 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*");
     }
-
-    /**
-     * Define o gerente de autenticação
-     *
-     * @author Giovane Neves
-     * @since Desde V1.0.1
-     * @param authenticationConfiguration Objeto de configurações de autenticação.
-     * @return o gerente de autenticação da aplicação.
-     */
-    @Bean
-    public AuthenticationManager
-    authenticationConfiguration(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    /**
-     * Define encoder de senhas.
-     *
-     * @author Giovane Neves
-     * @since Desde V1.0.1
-     * @return encoder de senha.
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-
-        return new BCryptPasswordEncoder();
-
-    }
-
 
 }

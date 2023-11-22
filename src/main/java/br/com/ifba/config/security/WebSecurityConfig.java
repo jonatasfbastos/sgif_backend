c68062583101cf3ae82e59e03ab4d43d8988671d
NekoYasha <autornekoyasha@gmail.com>
2023-11-22 10:29:13 -0300

    ✏ refactor: Renomeando métodos

--- src/main/java/br/com/ifba/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/ifba/config/security/WebSecurityConfig.java
@@ -1,31 +1,25 @@
 package br.com.ifba.config.security;
 
-import br.com.ifba.ApplicationContextLoad;
 import br.com.ifba.infrastructure.service.UserDetailsServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.HttpMethod;
-
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpSessionListener;
 
@@ -36,7 +30,7 @@ import javax.servlet.http.HttpSessionListener;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer, HttpSessionListener {
+public class WebSecurityConfig implements WebMvcConfigurer, HttpSessionListener {
 
 
     // =========================================================== //
@@ -44,26 +38,27 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvc
     // =========================================================== //
 
     @Autowired
+    private AuthenticationConfiguration configuration;
+
+    @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
     // =========================================================== //
-    // =============== [         ENDPOINTS      ] ================ //
+    // =============== [         MÉTODOS      ] ================== //
     // =========================================================== //
 
     /**
      * Configura construtor de gerenciador de autenticação.
      *
-     * @author Giovane Neves
-     * @since Desde V1.0.1
      * @param auth O construtor de gerenciador de autenticação.
      * @throws Exception caso haja algum erro na configuração.
+     * @author Giovane Neves
+     * @since Desde V1.0.1
      */
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
-
     }
 
     /**
@@ -75,7 +70,7 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvc
      * @return configurações da cadeia de segurança.
      */
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
 
         // TODO: Rever privacidade de endpoints POST.
 
@@ -90,7 +85,7 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvc
                     // Configuração de Logout.
                     try {
 
-                        auth.and().logout().logoutSuccessUrl("PATH_TO_LOGOUT_SCREEN");
+                        auth.and().logout().logoutSuccessUrl("/index");
                         auth.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 
                     } catch (Exception e) {
@@ -101,10 +96,11 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvc
 
                     try {
 
-                        // TODO: Resolver erro no authenticationManager().
-                        auth.and().addFilterAfter(new JWTLoginFilter(
-                                "/login",
-                                        ApplicationContextLoad.getApplicationContext().getBean(AuthenticationManager.class)),
+                        auth.and().addFilterAfter(
+                                new JWTLoginFilter(
+                                        "/login",
+                                        configuration.getAuthenticationManager()
+                                ),
                                 UsernamePasswordAuthenticationFilter.class
                         );
 
@@ -114,7 +110,8 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter implements WebMvc
                         throw new RuntimeException(e);
                     }
 
-                });
+                })
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
 

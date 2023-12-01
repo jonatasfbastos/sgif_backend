a231d9cd8cd48a34a9c8392ff5464bdc4b2b3741
NekoYasha <autornekoyasha@gmail.com>
2023-12-01 09:39:58 -0300

    ✏ refactor: Mudando de record para class

--- src/main/java/br/com/ifba/sgif/api/entity/perfilusuario/dto/PerfilUsuarioResponseDto.java
+++ src/main/java/br/com/ifba/sgif/api/entity/perfilusuario/dto/PerfilUsuarioResponseDto.java
@@ -2,30 +2,30 @@ package br.com.ifba.sgif.api.entity.perfilusuario.dto;
 
 import br.com.ifba.sgif.api.entity.perfilusuario.model.PerfilUsuario;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * DTO para resposta às requisições direcionadas à entidade 'Perfil de Usuário'.
  * Este DTO representa informações públicas do perfil de usuário, incluindo o nome e a descrição.
- *
- * @param nome - O nome do perfil de usuário.
- * @param descricao - A descrição do perfil de usuário.
- * 
+ * @author Andesson Reis, Giovane Neves
  * Veja também: {@link PerfilUsuario}
- * 
- * @author Andesson Reis
  */
 
-public record PerfilUsuarioResponseDto(
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PerfilUsuarioResponseDto {
 
-      /**
-       * O nome do perfil de usuário.
-       */
-      @JsonProperty(value = "nome") 
-      String nome,
-      /**
-       * A descrição do perfil de usuário.
-       */
-      @JsonProperty(value = "descricao")
-      String descricao
-    )
-{}
+    /**
+     * O nome do perfil de usuário.
+     */
+    @JsonProperty(value = "nome")
+    String nome;
+    /**
+     * A descrição do perfil de usuário.
+     */
+    @JsonProperty(value = "descricao")
+    String descricao;
+}
--- src/main/java/br/com/ifba/sgif/api/entity/usuario/dto/UsuarioResponseDto.java
+++ src/main/java/br/com/ifba/sgif/api/entity/usuario/dto/UsuarioResponseDto.java
@@ -3,34 +3,34 @@ package br.com.ifba.sgif.api.entity.usuario.dto;
 import br.com.ifba.sgif.api.entity.perfilusuario.dto.PerfilUsuarioResponseDto;
 import br.com.ifba.sgif.api.entity.usuario.model.Usuario;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
- * @author Andesson Reis
- * Desde V1.0.1
- * 
+ * @author Andesson Reis, Giovane Neves
+ * @since Desde V1.0.1
  * DTO para resposta às requisições direcionadas à entidade 'Usuário'.
  * Este DTO representa informações públicas do usuário, incluindo o login.
- *
- * @param login - O login do usuário (username)
- * @param senha - A senha do usuário
- * 
- * Veja também: {@link Usuario}
- * 
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
-public record UsuarioResponseDto(
-            /**
-             * O login do usuário (username).
-             */
-            @JsonProperty(value = "login")
-            String login,
-            /**
-             * A senha do usuário.
-             */
-            @JsonProperty(value = "senha") 
-            String senha,
+public class UsuarioResponseDto {
 
-            @JsonProperty(value = "perfilUsuario")
-            PerfilUsuarioResponseDto perfilUsuario
-            ) {
+    /**
+     * O login do usuário (username).
+     */
+    @JsonProperty(value = "login")
+    private String login;
+
+    /**
+     * A senha do usuário.
+     */
+    @JsonProperty(value = "senha")
+    private String senha;
+
+    @JsonProperty(value = "perfilUsuario")
+    private PerfilUsuarioResponseDto perfilUsuario;
 }
